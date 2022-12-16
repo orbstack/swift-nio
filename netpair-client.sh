d7037b0608a3601dd5466e9c68692611ccdcbfab
Danny Lin <danny@kdrag0n.dev>
2022-12-15 19:28:49 -0800

    l2 eth bridge

--- netpair-client.sh
+++ netpair-client.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 ip link set eth2 up
-ip addr add 192.168.99.2/24 dev eth2
+ip addr add 192.168.66.5/24 dev eth2
 
 # route via eth2
 ip route del default
-ip route add default via 192.168.99.1
+ip route add default via 192.168.66.1
+
+ip link set dev eth2 address 86:6c:f1:2e:9f:00
+
+
+# perf
+ip link set eth2 txqueuelen 10000
--- netpair-router.sh
+++ netpair-router.sh
@@ -6,11 +6,23 @@ echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
 
 killall udhcpc
 ip link set eth0 up
-udhcpc -i eth0
-
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+#udhcpc -i eth0
 
 ip link set eth2 up
-ip addr add 192.168.99.1/24 dev eth2
 
+ip link add name br0 type bridge
+ip link set eth0 master br0
+ip link set eth2 master br0
+#ip addr add 192.168.66.4/24 dev eth2
+ip addr add 192.168.66.3/24 dev br0
 ip link set eth1 down
+
+ip link set br0 up
+
+ip link set dev eth0 address 86:6c:f1:2e:9f:09
+
+
+# perf
+ip link set br0 txqueuelen 10000
+ip link set eth0 txqueuelen 10000
+ip link set eth2 txqueuelen 10000
