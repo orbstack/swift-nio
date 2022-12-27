6b43803b34ece6b4a792222fd5c0ba7f1a796e91
Danny Lin <danny@kdrag0n.dev>
2022-12-26 23:23:58 -0800

    delete more stuff

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -23,11 +23,6 @@ cp ../packages/*.pub etc/apk/keys/
 cp -r ../packages .
 arch-chroot . /bin/sh -l /build-inside.sh
 
-# VOID
-#tar xvf $HOME/Downloads/void-aarch64-musl-ROOTFS-20221001.tar.xz
-#cp ../build-inside-void.sh build-inside.sh
-#arch-chroot . /usr/bin/bash -l /build-inside.sh
-
 rm build-inside.sh
 rm -r packages
 
@@ -39,28 +34,11 @@ cp ../LICENSE .
 
 # preinit
 cp ../rd-compile/switch_overlay_root $OPT
-
-# netework: gvproxy
-cp ../rd-compile/lvproxy-guest $OPT
-cp $HOME/code/android/kvm/gvisor-tap-vsock/bin/vm $OPT/gvproxy-guest
-
-# network: passt tap
-# cp ../rd-compile/lvproxy-guest $OPT # still needed for 9p
-# cp ../../lvproxy/target/aarch64-unknown-linux-musl/debug/lvproxy-guest-tun $OPT
-
 # vcontrol server
 cp ../../vcontrol/target/aarch64-unknown-linux-musl/release/vcontrol $OPT
-
-# usb/ip
-cp -r ../rd-compile/usbip/prefix $OPT/usbip
-
 # debugging tools
 cp ../rd-compile/eventstat/eventstat $OPT
 
-# 9p proxy
-#cp ../rd-compile/9p-vsmount $OPT
-cp ../rd-compile/sshfs-vsmount $OPT
-cp ../rd-compile/sshfs-tcpmount $OPT
 
 # TODO handle ssh host keys
 cp ../../vcontainer86/rd/data/etc/ssh/ssh_host_* ./data/etc/ssh/
--- rootfs/pack.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "$0")"
-cd rd
-find . | cpio -o -H newc | gzip > ../initrd
-cd ..
-cp initrd ~/code/android/app/virtproto/app/src/main/assets/initrd
-#cp ../linux/kernel ~/code/android/app/virtproto/app/src/main/assets/kernel
-cp ~/code/android/kvm/linux/arch/arm64/boot/Image ~/code/android/app/virtproto/app/src/main/assets/kernel
