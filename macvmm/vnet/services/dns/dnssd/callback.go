af7082411a2fe2de961604539add1435cdf608f1
Danny Lin <danny@kdrag0n.dev>
2022-12-27 07:17:38 -0800

    dns logging back

--- macvmm/vnet/services/dns/dnssd/callback.go
+++ macvmm/vnet/services/dns/dnssd/callback.go
@@ -14,6 +14,7 @@ import (
 func go_dnssd_callback(context uint64, flags C.DNSServiceFlags, interfaceIndex C.uint32_t,
 	errorCode C.DNSServiceErrorType, fullname *C.char, rrtype C.uint16_t,
 	rrclass C.uint16_t, rdlen C.uint16_t, rdata unsafe.Pointer, ttl C.uint32_t) {
+	fmt.Printf("go_dnssd_callback(context=%v, flags=%v, interfaceIndex=%v, errorCode=%v, fullname=%v, rrtype=%v, rrclass=%v, rdlen=%v, rdata=%v, ttl=%v)\n", context, flags, interfaceIndex, errorCode, C.GoString(fullname), rrtype, rrclass, rdlen, rdata, ttl)
 
 	queryMapMu.RLock()
 	query, ok := queryMap[context]
@@ -40,8 +41,11 @@ func go_dnssd_callback(context uint64, flags C.DNSServiceFlags, interfaceIndex C
 	}
 	query.answers = append(query.answers, answer)
 
-	if flags&C.kDNSServiceFlagsMoreComing == 0 {
+	if flags&C.kDNSServiceFlagsMoreComing != 0 {
+		fmt.Printf("go_dnssd_callback: MoreComing\n")
+	} else {
 		query.done = true
 		C.DNSServiceRefDeallocate(query.ref)
 	}
+	fmt.Println("go_dnssd_callback: done")
 }
--- macvmm/vnet/services/dns/dnssd/dnssd.go
+++ macvmm/vnet/services/dns/dnssd/dnssd.go
@@ -64,6 +64,7 @@ func Query(name string, rtype uint16) ([]QueryAnswer, error) {
 		ref: sdRef,
 	}
 
+	fmt.Println("adding to map")
 	queryMapMu.Lock()
 	queryMap[queryId] = query
 	queryMapMu.Unlock()
@@ -83,12 +84,15 @@ func Query(name string, rtype uint16) ([]QueryAnswer, error) {
 			break
 		}
 
+		fmt.Println("  process")
 		ret := C.DNSServiceProcessResult(sdRef)
 		if ret != C.kDNSServiceErr_NoError {
+			fmt.Printf("  process result err %v\n", mapError(int(ret)))
 			return nil, mapError(int(ret))
 		}
 	}
 
+	fmt.Printf("done\n")
 	return query.answers, query.err
 }
 
@@ -96,6 +100,7 @@ func QueryRecursive(name string, rtype uint16) ([]QueryAnswer, error) {
 	// Keep CNAME at the top even if we're not looking for it
 	allAnswers := []QueryAnswer{}
 	for {
+		fmt.Println("\n\n********** RECURSE ROUND for", name, "**********")
 		newAnswers, err := Query(name, rtype)
 		if err != nil {
 			return nil, err
