f569db1a5c06eeda1f421d514f80c6c6b9420968
Danny Lin <danny@kdrag0n.dev>
2023-01-15 02:19:14 -0800

    fix2

--- macvmgr/vnet/tcpfwd/opts.go
+++ macvmgr/vnet/tcpfwd/opts.go
@@ -24,23 +24,25 @@ var (
 )
 
 func setExtNodelay(conn *net.TCPConn, otherPort int) error {
-	// take the chance to set keepalive too
-	err := conn.SetKeepAlive(false)
-	if err != nil {
-		return err
-	}
-
-	err = conn.SetNoDelay(false)
-	if err != nil {
-		return err
-	}
-
+	noDelay := false
 	extPort := conn.RemoteAddr().(*net.TCPAddr).Port
 	if _, ok := nodelayPorts[extPort]; ok {
-		return conn.SetNoDelay(true)
+		noDelay = true
 	}
 	if _, ok := nodelayPorts[otherPort]; ok {
-		return conn.SetNoDelay(true)
+		noDelay = true
 	}
+
+	err := conn.SetNoDelay(noDelay)
+	if err != nil {
+		return err
+	}
+
+	// take the chance to set keepalive too
+	err = conn.SetKeepAlive(false)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
