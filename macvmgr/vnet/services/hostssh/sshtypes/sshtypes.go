3a0d2075e8bf53dcb5078e75cafe15be0989ea0e
Danny Lin <danny@kdrag0n.dev>
2023-01-17 20:59:13 -0800

    fix obfuscation

--- macvmgr/vnet/services/hostssh/sshtypes/sshtypes.go
+++ macvmgr/vnet/services/hostssh/sshtypes/sshtypes.go
@@ -1,13 +1,13 @@
 package sshtypes
 
 type SshMeta struct {
-	Pwd        string
-	RawCommand bool
-	PtyStdin   bool
-	PtyStdout  bool
-	PtyStderr  bool
+	Pwd        string `json:"pwd"`
+	RawCommand bool   `json:"raw_command"`
+	PtyStdin   bool   `json:"pty_stdin"`
+	PtyStdout  bool   `json:"pty_stdout"`
+	PtyStderr  bool   `json:"pty_stderr"`
 }
 
 type MacMeta struct {
-	User string
+	User string `json:"user"`
 }
