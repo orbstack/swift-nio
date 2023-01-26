def4a46ddb34e0d245623c7ac2db08dbec117f54
Danny Lin <danny@kdrag0n.dev>
2023-01-25 17:39:53 -0800

    trampoline updates

--- rootfs/utils/trampoline.c
+++ rootfs/utils/trampoline.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <fcntl.h>
 
 int main(int argc, char** argv, char** envp) {
     char* fd_str = argv[1];
     int fd = atoi(fd_str);
+    fcntl(fd, F_SETFD, FD_CLOEXEC);
     fexecve(fd, argv + 2, envp);
     return errno;
 }
