f31aa474ce7551fc350b961542b12f23e4030757
Danny Lin <danny@kdrag0n.dev>
2023-01-30 23:46:49 -0800

    importswift

---
+++ swift/MacVirt/MacVirtApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacVirtApp.swift
+//  MacVirt
+//
+//  Created by Danny Lin on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MacVirtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
