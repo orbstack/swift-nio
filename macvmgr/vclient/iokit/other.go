47c454ac0b198a39ad3e0387d044400f4adec0d7
Danny Lin <danny@kdrag0n.dev>
2023-02-04 17:23:34 -0800

    o

--- macvmgr/vclient/iokit/other.go
+++ macvmgr/vclient/iokit/other.go
@@ -2,6 +2,11 @@
 
 package iokit
 
-func MonitorSleepWake() (chan time.Time, error) {
+type SleepWakeMonitor struct {
+	SleepChan chan time.Time
+	WakeChan  chan time.Time
+}
+
+func MonitorSleepWake() (*SleepWakeMonitor, error) {
 	return nil, errors.New("not implemented")
 }
