6653eeb574e23188159504bcf0eb6e5518109087
Danny Lin <danny@kdrag0n.dev>
2023-02-17 18:05:57 -0800

    upd

--- swift/guihelper/main.swift
+++ swift/guihelper/main.swift
@@ -6,6 +6,31 @@
 //
 
 import Foundation
+import AppKit
 
-print("Hello, World!")
+func mainNotify(_ args: [String]) -> Int32 {
+    let notification = NSUserNotification()
+    notification.title = args[0]
+    notification.informativeText = args[1]
 
+    if args[2] == "--sound" {
+        notification.soundName = NSUserNotificationDefaultSoundName
+    }
+    if args[3] != "" {
+        notification.subtitle = args[3]
+    }
+
+    NSUserNotificationCenter.default.scheduleNotification(notification)
+    RunLoop.current.run(until: Date.init(timeIntervalSinceNow: 0.1))
+    return 0
+}
+
+let targetCmd = CommandLine.arguments[1]
+let args = CommandLine.arguments.dropFirst(2).map { String($0) }
+switch targetCmd {
+case "notify":
+    exit(mainNotify(args))
+default:
+    print("Unknown command")
+    exit(1)
+}
