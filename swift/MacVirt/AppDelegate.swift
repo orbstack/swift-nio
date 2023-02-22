88bb0d21980d540ff3c9ee423a6ed0f4276e3cc5
Danny Lin <danny@kdrag0n.dev>
2023-02-22 02:19:57 -0800

    dsn

--- swift/MacVirt/AppDelegate.swift
+++ swift/MacVirt/AppDelegate.swift
@@ -12,7 +12,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         if !AppConfig.c.debug {
             SentrySDK.start { options in
-                options.dsn = "https://e97d84d2f3ad48dbac3c491be1ad4c52@o120089.ingest.sentry.io/4504665519554560"
+                options.dsn = "https://e115a1e5bb7a453f93fada4fadc4c3ac@o120089.ingest.sentry.io/4504665519554560"
                 options.tracesSampleRate = 0.0
                 options.enableAppHangTracking = false
             }
