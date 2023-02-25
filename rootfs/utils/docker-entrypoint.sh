e9d0d0daf302d50daff0aeba9859c07e2e94c08b
Danny Lin <danny@kdrag0n.dev>
2023-02-25 03:51:23 -0800

    Docker Desktop ssh agent compat: /run/host-services/ssh-auth.sock

--- rootfs/utils/docker-entrypoint.sh
+++ rootfs/utils/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 set -e
+# Docker Desktop compat
+mkdir -p /run/host-services
+ln -sf /opt/orbstack-guest/run/host-ssh-agent.sock /run/host-services/ssh-auth.sock
 ip6tables -t nat -A POSTROUTING -s fc00:30:32::/64 -o eth0 -j MASQUERADE
 exec dockerd --host=unix:///var/run/docker.sock --tls=false
