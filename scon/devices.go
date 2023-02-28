bc7f8f72356766046b0f25e0276a02554d8ebbfb
Danny Lin <danny@kdrag0n.dev>
2023-02-27 23:16:19 -0800

    log

--- scon/devices.go
+++ scon/devices.go
@@ -19,6 +19,10 @@ func (m *ConManager) addDeviceNodeAll(src string, dst string) error {
 			continue
 		}
 
+		logrus.WithFields(logrus.Fields{
+			"container": c.Name,
+			"dst":       dst,
+		}).Debug("adding device node")
 		err := c.addDeviceNode(src, dst)
 		if err != nil && !errors.Is(err, lxc.ErrNotRunning) {
 			errs = append(errs, err)
@@ -38,6 +42,10 @@ func (m *ConManager) removeDeviceNodeAll(src string, dst string) error {
 			continue
 		}
 
+		logrus.WithFields(logrus.Fields{
+			"container": c.Name,
+			"dst":       dst,
+		}).Debug("removing device node")
 		err := c.removeDeviceNode(src, dst)
 		if err != nil && !errors.Is(err, lxc.ErrNotRunning) {
 			errs = append(errs, err)
@@ -53,13 +61,13 @@ func (m *ConManager) handleDeviceEvent(event fsnotify.Event) {
 	nodeName := path.Base(event.Name)
 	if MatchesExtraDevice(nodeName) {
 		if event.Op&fsnotify.Create != 0 {
-			logrus.WithField("path", event.Name).Debug("extra device created")
+			logrus.WithField("path", event.Name).Debug("creating extra device")
 			err := m.addDeviceNodeAll(event.Name, event.Name)
 			if err != nil {
 				logrus.WithError(err).Error("failed to add extra device")
 			}
 		} else if event.Op&fsnotify.Remove != 0 {
-			logrus.WithField("path", event.Name).Debug("extra device removed")
+			logrus.WithField("path", event.Name).Debug("removing extra device")
 			err := m.removeDeviceNodeAll(event.Name, event.Name)
 			if err != nil {
 				logrus.WithError(err).Error("failed to remove extra device")
