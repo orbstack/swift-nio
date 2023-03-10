7f540923d0bcc3ff496e966da38b677ff0ebe887
Danny Lin <danny@kdrag0n.dev>
2023-03-09 23:20:23 -0800

    fix pub script

--- publish-update.sh
+++ publish-update.sh
@@ -2,25 +2,20 @@
 
 set -euxo pipefail
 
-LONG_VER=$(git describe --tags --always --dirty)
-COMMITS=$(git rev-list --count HEAD)
-
 cd "$(dirname "$0")"
 
 pushd swift/out
-
 built_dmgs=(*/*.dmg)
-
 popd
 
 # updates
 SPARKLE_BIN=~/Library/Developer/Xcode/DerivedData/MacVirt-cvlazugpvgfgozfesiozsrqnzfat/SourcePackages/artifacts/sparkle/bin
-mkdir -p updates/{arm64,amd64}
-cp swift/out/arm64/*.dmg updates/arm64/ || :
-cp swift/out/amd64/*.dmg updates/amd64/ || :
+mkdir -p updates/pub/{arm64,amd64}
+cp swift/out/arm64/*.dmg updates/pub/arm64/ || :
+cp swift/out/amd64/*.dmg updates/pub/amd64/ || :
 COMMON_FLAGS=(--channel beta --critical-update-version '' --auto-prune-update-files --delta-compression lzfse --release-notes-url-prefix 'https://cdn-updates.orbstack.dev/release-notes.html#' --full-release-notes-url 'https://docs.orbstack.dev/release-notes' --maximum-versions 2 --maximum-deltas 3)
-$SPARKLE_BIN/generate_appcast "${COMMON_FLAGS[@]}" --download-url-prefix https://cdn-updates.orbstack.dev/arm64/ updates/arm64
-$SPARKLE_BIN/generate_appcast "${COMMON_FLAGS[@]}" --download-url-prefix https://cdn-updates.orbstack.dev/amd64/ updates/amd64
+$SPARKLE_BIN/generate_appcast "${COMMON_FLAGS[@]}" --download-url-prefix https://cdn-updates.orbstack.dev/arm64/ updates/pub/arm64
+$SPARKLE_BIN/generate_appcast "${COMMON_FLAGS[@]}" --download-url-prefix https://cdn-updates.orbstack.dev/amd64/ updates/pub/amd64
 
 # upload to cloudflare
-rclone sync updates r2:orbstack-updates
+rclone sync updates/pub r2:orbstack-updates
