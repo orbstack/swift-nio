fefa498948b302c821e614ec9a9c4b6a726e41f0
Danny Lin <danny@kdrag0n.dev>
2023-03-26 05:52:32 -0700

    Revert "only exit on exec delete"
    This reverts commit fa70ed5ad42b35ff6f3e4a3146fb885eb164ac19.
    
    i don't trust this to work correctly during update

--- macvmgr/stop_notify.go
+++ macvmgr/stop_notify.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/kdrag0n/macvirt/macvmgr/conf"
@@ -36,6 +37,17 @@ func WatchCriticalFiles(stopCh chan<- StopType) error {
 		return err
 	}
 
+	// resolve volume if we're on one
+	var volumePath string
+	if strings.HasPrefix(execPath, "/Volumes/") {
+		volumeName := strings.Split(execPath, "/")[2]
+		volumePath = "/Volumes/" + volumeName
+		err = watcher.AddWithEvents(volumePath, unix.NOTE_DELETE)
+		if err != nil {
+			return err
+		}
+	}
+
 	for {
 		select {
 		case event := <-watcher.Events:
@@ -43,11 +55,17 @@ func WatchCriticalFiles(stopCh chan<- StopType) error {
 			if event.Op&fsnotify.Remove == fsnotify.Remove {
 				if event.Name == execPath {
 					logrus.Info("Executable deleted, exiting")
+					// we could hang forver if we access any uncached code pages
+					os.Exit(1)
+				} else if event.Name == volumePath {
+					logrus.Info("DMG volume ejected, exiting")
+					// we could hang forver if we access any uncached code pages
+					os.Exit(1)
 				} else {
 					logrus.Info("Data image deleted, stopping VM")
+					// force is ok - data doesn't matter anymore
+					stopCh <- StopForce
 				}
-				// force is ok - data doesn't matter anymore
-				stopCh <- StopForce
 				return nil
 			}
 		case err := <-watcher.Errors:
