2856923da990c77a009185f566d293cbe9cc079d
Danny Lin <danny@kdrag0n.dev>
2023-03-26 19:44:32 -0700

    stop on brew uninstall

--- scon/cmd/scli/cmd/internal_brew_uninstall.go
+++ scon/cmd/scli/cmd/internal_brew_uninstall.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"github.com/kdrag0n/macvirt/macvmgr/vmclient"
+	"github.com/kdrag0n/macvirt/scon/cmd/scli/spinutil"
 	"github.com/spf13/cobra"
 )
 
@@ -17,6 +19,22 @@ var internalBrewUninstall = &cobra.Command{
 	Use:    "brew-uninstall",
 	Hidden: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if !vmclient.IsRunning() {
+			return nil
+		}
+
+		spinner := spinutil.Start("red", "Cleaning up")
+		var err error
+		if flagZap {
+			// force stop if zap
+			err = vmclient.Client().SyntheticForceStopOrKill()
+		} else {
+			// otherwise graceful stop
+			err = vmclient.Client().Stop()
+		}
+		spinner.Stop()
+		checkCLI(err)
+
 		return nil
 	},
 }
