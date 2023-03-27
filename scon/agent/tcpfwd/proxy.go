f670ca1d78623dcbce273c9d0e8a2e948db13e21
Danny Lin <danny@kdrag0n.dev>
2023-03-27 16:27:23 -0700

    proxy: retry localhost v4v6  conn w/ other protocol

--- scon/agent/tcpfwd/proxy.go
+++ scon/agent/tcpfwd/proxy.go
@@ -6,6 +6,10 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+var (
+	ipv4Loopback = net.IPv4(127, 0, 0, 1)
+)
+
 type TCPProxy struct {
 	listener net.Listener
 	isIPv6   bool
@@ -35,19 +39,32 @@ func (p *TCPProxy) handleConn(conn net.Conn) {
 	defer conn.Close()
 
 	// dial
-	dialAddr := *&net.TCPAddr{
+	dialAddr := net.TCPAddr{
 		Port: int(p.port),
 	}
 	if p.isIPv6 {
 		dialAddr.IP = net.IPv6loopback
 	} else {
-		dialAddr.IP = net.IPv4(127, 0, 0, 1)
+		dialAddr.IP = ipv4Loopback
 	}
 
 	dialConn, err := net.DialTCP("tcp", nil, &dialAddr)
 	if err != nil {
-		logrus.WithError(err).Error("failed to dial local")
-		return
+		logrus.WithError(err).Error("failed to dial local (1)")
+
+		// if conn refused (i.e. no listener) but our proxy is still registered,
+		// try dialing the other v4/v6 protocol
+		if p.isIPv6 {
+			dialAddr.IP = ipv4Loopback
+		} else {
+			dialAddr.IP = net.IPv6loopback
+		}
+		logrus.WithField("dialAddr", dialAddr).Debug("retrying with other protocol")
+		dialConn, err = net.DialTCP("tcp", nil, &dialAddr)
+		if err != nil {
+			logrus.WithError(err).Error("failed to dial local (2)")
+			return
+		}
 	}
 	defer dialConn.Close()
 
