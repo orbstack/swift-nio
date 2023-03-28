18caf55394b5aac82acf6b3973c5abcdfec66e54
Danny Lin <danny@kdrag0n.dev>
2023-03-28 01:03:45 -0700

    logrus with

--- macvmgr/vnet/tcpfwd/pump.go
+++ macvmgr/vnet/tcpfwd/pump.go
@@ -39,11 +39,11 @@ func pump2(c1, c2 FullDuplexConn) {
 
 	// Don't wait for both if one side failed (not EOF)
 	if err1 := <-errChan; err1 != nil {
-		logrus.Error("tcp pump2 error 1 ", err1)
+		logrus.WithError(err1).Error("tcp pump2 error 1")
 		return
 	}
 	if err2 := <-errChan; err2 != nil {
-		logrus.Error("tcp pump2 error 2 ", err2)
+		logrus.WithError(err2).Error("tcp pump2 error 2")
 		return
 	}
 }
