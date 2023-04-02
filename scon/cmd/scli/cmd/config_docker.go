6f045684f0284afe7fd8cf22cc0359072f6ff9dd
Danny Lin <danny@kdrag0n.dev>
2023-04-02 07:03:29 -0700

    add docker config command

---
+++ scon/cmd/scli/cmd/config_docker.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"os"
+	"os/exec"
+
+	"github.com/kdrag0n/macvirt/macvmgr/conf"
+	"github.com/kdrag0n/macvirt/macvmgr/conf/appid"
+	"github.com/kdrag0n/macvirt/macvmgr/vmclient"
+	"github.com/kdrag0n/macvirt/scon/cmd/scli/scli"
+	"github.com/kdrag0n/macvirt/scon/cmd/scli/spinutil"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	configCmd.AddCommand(configDockerCmd)
+}
+
+var configDockerCmd = &cobra.Command{
+	Use:   "docker",
+	Short: "Edit Docker engine configuration",
+	Long: `Open the Docker engine configuration file for editing.
+
+This will open ~/.orbstack/config/docker.json in your default command line text editor ($EDITOR).
+If changes are made, the Docker engine will be restarted.
+`,
+	Example: "  " + appid.ShortCtl + " docker",
+	Args:    cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		configPath := conf.DockerDaemonConfig()
+		preferredEditor := os.Getenv("EDITOR")
+		if preferredEditor == "" {
+			preferredEditor = "nano"
+		}
+
+		// read preimage
+		preConfig, err := os.ReadFile(configPath)
+		checkCLI(err)
+
+		eCmd := exec.Command(preferredEditor, configPath)
+		eCmd.Stdin = os.Stdin
+		eCmd.Stdout = os.Stdout
+		eCmd.Stderr = os.Stderr
+		err = eCmd.Run()
+		checkCLI(err)
+
+		// read postimage
+		postConfig, err := os.ReadFile(configPath)
+		checkCLI(err)
+
+		// restart if changed and vm is running
+		if string(preConfig) != string(postConfig) && vmclient.IsRunning() {
+			// ensure scon
+			scli.EnsureSconVMWithSpinner()
+
+			// restart docker
+			record, err := scli.Client().GetByName("docker")
+			checkCLI(err)
+			spinner := spinutil.Start("green", "Restarting Docker")
+			err = scli.Client().ContainerRestart(record)
+			spinner.Stop()
+			checkCLI(err)
+		}
+
+		return nil
+	},
+}
