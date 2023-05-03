b749a163711f90148dddd41a7bc732f271afd068
Danny Lin <danny@kdrag0n.dev>
2023-05-03 08:35:05 -0700

    version

---
+++ scon/cmd/scli/cmd/version.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/kdrag0n/macvirt/macvmgr/conf/appid"
+	"github.com/kdrag0n/macvirt/macvmgr/conf/appver"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show OrbStack version",
+	Long: `Show OrbStack version information.
+`,
+	Example: "  " + appid.ShortCtl + " version",
+	Args:    cobra.ArbitraryArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ver := appver.Get()
+		fmt.Printf("Version: %s (%d)\n", ver.Short, ver.Code)
+		fmt.Printf("Commit: %s (%s)\n", ver.GitCommit, ver.GitDescribe)
+
+		return nil
+	},
+}
