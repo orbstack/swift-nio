8030794deb793ff0859227d9ee709edb961dfd19
Danny Lin <danny@kdrag0n.dev>
2023-06-05 21:42:12 -0700

    report kernel panic to sentry

--- macvmgr/log_collector.go
+++ macvmgr/log_collector.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"errors"
 	"io"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/fatih/color"
+	"github.com/getsentry/sentry-go"
 )
 
 const (
 	panicShutdownDelay = 100 * time.Millisecond
 )
+
+type KernelPanicError struct {
+	Err error
+}
+
+func (e *KernelPanicError) Error() string {
+	return e.Err.Error()
+}
 
 func isMultibyteByte(firstByte byte) bool {
 	return firstByte >= 0x80
@@ -29,6 +40,8 @@ func NewConsoleLogPipe(stopCh chan<- StopType) (*os.File, error) {
 	magenta := color.New(color.FgMagenta).SprintFunc()
 	yellow := color.New(color.FgYellow).SprintFunc()
 
+	var panicBuffer *bytes.Buffer
+
 	go func() {
 		defer w.Close()
 		// copy each line and prefix it
@@ -40,13 +53,25 @@ func NewConsoleLogPipe(stopCh chan<- StopType) (*os.File, error) {
 			if len(line) > 0 && isMultibyteByte(line[0]) {
 				io.WriteString(os.Stdout, line)
 			} else if len(line) > 0 && line[0] == '[' {
-				io.WriteString(os.Stdout, kernelPrefix+magenta(line))
-
 				// shut down on kernel panic
 				if strings.Contains(line, "] Kernel panic - not syncing:") {
+					// start recording panic lines
+					panicBuffer = new(bytes.Buffer)
+
 					time.AfterFunc(panicShutdownDelay, func() {
 						stopCh <- StopForce
+
+						// report panic lines to sentry
+						sentry.CaptureException(&KernelPanicError{
+							Err: errors.New("kernel panic:\n" + panicBuffer.String()),
+						})
 					})
+				}
+
+				io.WriteString(os.Stdout, kernelPrefix+magenta(line))
+
+				if panicBuffer != nil {
+					panicBuffer.WriteString(line)
 				}
 			} else {
 				io.WriteString(os.Stdout, consolePrefix+yellow(line))
