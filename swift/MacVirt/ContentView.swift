d7258fb3207e5363564065f34ef08fd776500c58
Danny Lin <danny@kdrag0n.dev>
2023-06-07 00:54:55 -0700

    fix macos 13 selection

--- swift/MacVirt/ContentView.swift
+++ swift/MacVirt/ContentView.swift
@@ -37,46 +37,49 @@ struct ContentView: View {
 
     private var sidebarContents12: some View {
         Group {
+            // on macOS 14, must put .tag() on Label or it crashes
+            // on macOS <=13, must put .tag() on NavigationLink or it doesn't work
             Section(header: Text("Docker")) {
-                NavigationLink(destination: DockerContainersRootView(initialSelection: initialDockerContainerSelection, selection: initialDockerContainerSelection, searchQuery: "")) {
+                NavigationLink(destination: DockerContainersRootView(initialSelection: initialDockerContainerSelection,
+                                                                     selection: initialDockerContainerSelection, searchQuery: "")) {
                     Label("Containers", systemImage: "shippingbox")
                         .padding(.vertical, 3)
-                        .tag("docker")
                 }
+                .tag("docker")
                 
                 NavigationLink(destination: DockerVolumesRootView()) {
                     Label("Volumes", systemImage: "externaldrive")
                         .padding(.vertical, 3)
-                        .tag("docker-volumes")
                 }
+                .tag("docker-volumes")
                 
                 NavigationLink(destination: DockerImagesRootView()) {
                     Label("Images", systemImage: "doc.zipper")
                         .padding(.vertical, 3)
-                        .tag("docker-images")
                 }
+                .tag("docker-images")
             }
             
             Section(header: Text("Linux")) {
                 NavigationLink(destination: MachinesRootView()) {
                     Label("Machines", systemImage: "desktopcomputer")
                         .padding(.vertical, 3)
-                        .tag("machines")
                 }
+                .tag("machines")
                 
                 NavigationLink(destination: FilesRootView()) {
                     Label("Files", systemImage: "folder")
                         .padding(.vertical, 3)
-                        .tag("files")
                 }
+                .tag("files")
             }
             
             Section(header: Text("Info")) {
                 NavigationLink(destination: CommandsRootView()) {
                     Label("Commands", systemImage: "terminal")
                         .padding(.vertical, 3)
-                        .tag("cli")
                 }
+                .tag("cli")
             }
         }
     }
