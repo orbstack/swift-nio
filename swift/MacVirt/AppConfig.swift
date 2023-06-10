6025dda54c9aa227a15c6d64819fa4a6e4acc372
Danny Lin <danny@kdrag0n.dev>
2023-06-10 02:28:31 -0700

    fix debug exe

--- swift/MacVirt/AppConfig.swift
+++ swift/MacVirt/AppConfig.swift
@@ -15,16 +15,13 @@ struct AppConfig {
 #if DEBUG
     static let debug = true
     static let vmgrExe = "\(Folders.home)/code/projects/macvirt/macvmgr/OrbStack Helper (VM)"
-    static let shellExe = "\(Folders.home)/code/projects/macvirt/macvmgr/bin/orb"
-    static let dockerExe = "\(Folders.home)/code/projects/macvirt/macvmgr/xbin/docker"
-    static let dockerComposeExe = "\(Folders.home)/code/projects/macvirt/macvmgr/xbin/docker-compose"
 #else
     static let debug = false
     static let vmgrExe = pathForAuxiliaryExecutable("OrbStack Helper (VM)")
+#endif
     static let shellExe = pathForAuxiliaryExecutable("bin/orb")
     static let dockerExe = pathForAuxiliaryExecutable("xbin/docker")
     static let dockerComposeExe = pathForAuxiliaryExecutable("xbin/docker-compose")
-#endif
 }
 
 // can't crash because bundlePath can't be nil
