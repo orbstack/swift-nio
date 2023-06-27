064822886244cb3d29bf3020cf0e95c292f3e488
Danny Lin <danny@kdrag0n.dev>
2023-06-27 08:20:01 -0700

    copy report to clipboard

--- scon/cmd/scli/cmd/report.go
+++ scon/cmd/scli/cmd/report.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"os"
+	"os/exec"
 	"runtime"
 
+	"github.com/fatih/color"
 	"github.com/orbstack/macvirt/scon/cmd/scli/osutil"
 	"github.com/orbstack/macvirt/vmgr/conf/appid"
 	"github.com/orbstack/macvirt/vmgr/conf/appver"
@@ -24,31 +29,49 @@ Issue tracker: https://github.com/orbstack/orbstack/issues
 	Example: "  " + appid.ShortCtl + " report",
 	Args:    cobra.ArbitraryArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("OrbStack info:")
-		ver := appver.Get()
-		fmt.Printf("  Version: %s (%d)\n", ver.Short, ver.Code)
-		fmt.Printf("  Commit: %s (%s)\n", ver.GitCommit, ver.GitDescribe)
-		fmt.Println("")
+		var buffer bytes.Buffer
+		writer := io.MultiWriter(os.Stdout, &buffer)
 
-		fmt.Println("System info:")
+		fmt.Fprintln(writer, "OrbStack info:")
+		ver := appver.Get()
+		fmt.Fprintf(writer, "  Version: %s (%d)\n", ver.Short, ver.Code)
+		fmt.Fprintf(writer, "  Commit: %s (%s)\n", ver.GitCommit, ver.GitDescribe)
+		fmt.Fprintln(writer, "")
+
+		fmt.Fprintln(writer, "System info:")
 		osVerCode, err := osutil.OsVersionCode()
 		checkCLI(err)
 		osProductVer, err := osutil.OsProductVersion()
 		checkCLI(err)
-		fmt.Printf("  macOS: %s (%s)\n", osProductVer, osVerCode)
-		fmt.Printf("  CPU: %s, %d cores\n", runtime.GOARCH, runtime.NumCPU())
+		fmt.Fprintf(writer, "  macOS: %s (%s)\n", osProductVer, osVerCode)
+		fmt.Fprintf(writer, "  CPU: %s, %d cores\n", runtime.GOARCH, runtime.NumCPU())
 		cpuModel, err := osutil.CpuModel()
 		checkCLI(err)
-		fmt.Printf("  CPU model: %s\n", cpuModel)
-		fmt.Println("")
+		fmt.Fprintf(writer, "  CPU model: %s\n", cpuModel)
+		fmt.Fprintln(writer, "")
 
 		// TODO: settings
 
-		fmt.Println("---------------- [ cut here ] ----------------")
-		fmt.Println("")
-		fmt.Println("Please copy and paste the above information into your bug report.")
-		fmt.Println("Open an issue here: https://github.com/orbstack/orbstack/issues/new/choose")
-		fmt.Println("")
+		// stop writing to buffer after this point
+		writer = os.Stdout
+
+		fmt.Fprintln(writer, "---------------- [ cut here ] ----------------")
+		fmt.Fprintln(writer, "")
+		fmt.Fprintln(writer, "Please copy and paste the above information into your bug report.")
+		fmt.Fprintln(writer, "Open an issue here: https://github.com/orbstack/orbstack/issues/new/choose")
+		fmt.Fprintln(writer, "")
+
+		// copy to clipboard
+		copyCmd := exec.Command("pbcopy")
+		copyCmd.Stdin = &buffer
+		err = copyCmd.Run()
+		if err != nil {
+			fmt.Fprintln(writer, "Failed to copy to clipboard:", err)
+		}
+
+		// print copied
+		greenBold := color.New(color.FgGreen, color.Bold).SprintFunc()
+		fmt.Fprintln(writer, greenBold("✅ Copied to clipboard!"))
 
 		return nil
 	},
