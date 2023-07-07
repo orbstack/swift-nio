b52eb622070022fa4958930d7b2dee296e2b734d
Danny Lin <danny@kdrag0n.dev>
2023-07-07 07:01:18 -0700

    dynamic bg setting label

--- swift/MacVirt/Settings/GeneralSettingsView.swift
+++ swift/MacVirt/Settings/GeneralSettingsView.swift
@@ -12,13 +12,21 @@ import Defaults
 struct GeneralSettingsView: View {
     let updaterController: SPUStandardUpdaterController
 
+    @Default(.globalShowMenubarExtra) private var showMenubarExtra
+
     var body: some View {
         Form {
             LaunchAtLogin.Toggle {
                 Text("Start at login")
             }
             Defaults.Toggle("Show in menu bar", key: .globalShowMenubarExtra)
-            Defaults.Toggle("Stay in background when app is quit", key: .globalStayInBackground)
+            // ZStack to avoid layout shift
+            ZStack(alignment: .leading) {
+                Defaults.Toggle("Keep running when menu bar app is quit", key: .globalStayInBackground)
+                    .opacity(showMenubarExtra ? 1 : 0)
+                Defaults.Toggle("Keep running when app is quit", key: .globalStayInBackground)
+                    .opacity(showMenubarExtra ? 0 : 1)
+            }
 
             Spacer()
                     .frame(height: 20)
