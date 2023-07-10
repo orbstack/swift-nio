39c936badb30ba70a4d3212e784812ecd0e98293
Danny Lin <danny@kdrag0n.dev>
2023-07-09 21:08:40 -0700

    add back 172.17.0.1 compat gateway redirect

--- rootfs/utils/docker-entrypoint.sh
+++ rootfs/utils/docker-entrypoint.sh
@@ -12,5 +12,6 @@ mkdir -p /run/host-services
 ln -sf /opt/orbstack-guest/run/host-ssh-agent.sock /run/host-services/ssh-auth.sock
 
 ip6tables -t nat -A POSTROUTING -s fd07:b51a:cc66:0001::/64 -o eth0 -j MASQUERADE
+iptables -t nat -A PREROUTING -s 192.168.228.0/23 -d 172.17.0.1 -i docker0 -j DNAT --to-destination 192.168.228.1
 export TMPDIR=/dockertmp
 exec dockerd --host-gateway-ip=198.19.248.254
