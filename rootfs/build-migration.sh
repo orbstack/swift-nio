a556adceb5734943dced63fcdf5379eae5395085
Danny Lin <danny@kdrag0n.dev>
2023-07-21 16:05:30 -0700

    remove apk tools

--- rootfs/build-migration.sh
+++ rootfs/build-migration.sh
@@ -6,3 +6,5 @@ cd "$(dirname "$0")"
 
 docker build --platform linux/arm64 -f migration/Dockerfile .. -t ghcr.io/orbstack/dmigrate-agent:1
 docker build --platform linux/amd64 -f migration/Dockerfile .. -t ghcr.io/orbstack/dmigrate-agent:1
+
+#docker push ghcr.io/orbstack/dmigrate-agent:1
