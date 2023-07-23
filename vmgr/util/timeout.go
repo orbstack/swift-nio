893f560464f9461c2f403e2475baa03e26f7a943
Danny Lin <danny@kdrag0n.dev>
2023-07-23 04:42:31 -0700

    new timeout impl w/ tests

--- vmgr/util/timeout.go
+++ vmgr/util/timeout.go
@@ -1,32 +1,21 @@
 package util
 
 import (
-	"errors"
+	"context"
 	"time"
 )
 
-var (
-	ErrFnTimeout = errors.New("func timeout")
-)
-
 func WithTimeout[T any](fn func() (T, error), timeout time.Duration) (T, error) {
-	// wil be GC'd once both are done
-	done := make(chan struct{}, 1)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
 
 	var result T
-	var err error
+	err := context.DeadlineExceeded
 	go func() {
 		result, err = fn()
-		done <- struct{}{}
+		cancel()
 	}()
 
-	timer := time.NewTimer(timeout)
-	defer timer.Stop()
-
-	select {
-	case <-done:
-		return result, err
-	case <-timer.C:
-		return result, ErrFnTimeout
-	}
+	<-ctx.Done()
+	return result, err
 }
---
+++ vmgr/util/timeout_test.go
@@ -0,0 +1,57 @@
+package util
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+)
+
+var (
+	errTest = errors.New("test error")
+)
+
+func TestTimeoutOk(t *testing.T) {
+	fn := func() (string, error) {
+		return "ok", nil
+	}
+	result, err := WithTimeout(fn, 1*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result != "ok" {
+		t.Fatal("result is not ok")
+	}
+}
+
+func TestTimeoutExceeded(t *testing.T) {
+	fn := func() (string, error) {
+		time.Sleep(2 * time.Second)
+		return "ok", nil
+	}
+	_, err := WithTimeout(fn, 1*time.Second)
+	if err != context.DeadlineExceeded {
+		t.Fatal("expected DeadlineExceeded error")
+	}
+}
+
+func TestTimeoutError(t *testing.T) {
+	fn := func() (string, error) {
+		return "", errTest
+	}
+	_, err := WithTimeout(fn, 1*time.Second)
+	if err != errTest {
+		t.Fatal("expected test error")
+	}
+}
+
+func TestTimeoutErrorAfterTimeout(t *testing.T) {
+	fn := func() (string, error) {
+		time.Sleep(2 * time.Second)
+		return "", errTest
+	}
+	_, err := WithTimeout(fn, 1*time.Second)
+	if err != context.DeadlineExceeded {
+		t.Fatal("expected DeadlineExceeded error")
+	}
+}
