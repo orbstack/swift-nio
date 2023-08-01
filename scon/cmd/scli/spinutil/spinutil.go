b424172d597dbaffa8f1ba531caddc2b6b8c834e
Danny Lin <danny@kdrag0n.dev>
2023-08-01 15:15:05 -0700

    restore cursor state after ^C spinner

--- scon/cmd/scli/spinutil/spinutil.go
+++ scon/cmd/scli/spinutil/spinutil.go
@@ -1,13 +1,19 @@
 package spinutil
 
 import (
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/briandowns/spinner"
+	"github.com/fatih/color"
+	"golang.org/x/sys/unix"
+	"golang.org/x/term"
 )
 
 type Spinner struct {
-	*spinner.Spinner
+	spinner *spinner.Spinner
+	stopCh  chan struct{}
 }
 
 func Start(color string, message string) *Spinner {
@@ -16,9 +22,33 @@ func Start(color string, message string) *Spinner {
 	spin.Suffix = " " + message
 	spin.Start()
 
-	return &Spinner{spin}
+	sp := &Spinner{
+		spinner: spin,
+		stopCh:  make(chan struct{}),
+	}
+	go sp.monitorSignals()
+	return sp
+}
+
+func (s *Spinner) monitorSignals() {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, unix.SIGINT, unix.SIGTERM, unix.SIGQUIT)
+	defer signal.Stop(sigCh)
+
+	select {
+	case sig := <-sigCh:
+		s.Stop()
+		if term.IsTerminal(int(os.Stderr.Fd())) {
+			color.New(color.FgRed).Fprintln(os.Stderr, "Canceled")
+		}
+		// simulate the signal exit
+		signal.Stop(sigCh)
+		unix.Kill(os.Getpid(), sig.(unix.Signal))
+	case <-s.stopCh:
+	}
 }
 
 func (s *Spinner) Stop() {
-	s.Spinner.Stop()
+	s.spinner.Stop()
+	close(s.stopCh)
 }
