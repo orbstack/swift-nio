03c0602c68255d46a6c45ff8b4420678126478f2
Danny Lin <danny@kdrag0n.dev>
2023-08-06 05:15:24 -0700

    move vmnet host ip calculation to sgtypes

--- scon/sgclient/sgtypes/sgtypes.go
+++ scon/sgclient/sgtypes/sgtypes.go
@@ -1,6 +1,7 @@
 package sgtypes
 
 import (
+	"net"
 	"net/netip"
 
 	"github.com/orbstack/macvirt/vmgr/dockertypes"
@@ -16,7 +17,64 @@ type DockerBridgeConfig struct {
 	GuestInterfaceName string
 }
 
+func (config *DockerBridgeConfig) HostIP4() (net.IP, net.IPMask) {
+	mask := prefixToMask(config.IP4Subnet)
+	ip := net.IP(config.IP4Subnet.Addr().AsSlice())
+	// last IP - to avoid conflict with containers or gateway
+	ip = lastIPInSubnet(ip, mask)
+	// if it conflicts with the Linux-side host/gateway IP (bip), subtract 1 more from last octet
+	if ip.Equal(config.IP4Gateway.AsSlice()) {
+		ip[len(ip)-1]--
+	}
+
+	return ip, mask
+}
+
+func (config *DockerBridgeConfig) HostIP6() (net.IP, net.IPMask) {
+	mask := prefixToMask(config.IP6Subnet)
+	ip := net.IP(config.IP6Subnet.Addr().AsSlice())
+	// last IP - to avoid conflict with containers or gateway
+	// this has basically no chance of conflicting with a user-selected or SLAAC IP,
+	// and Docker doesn't provide gateway IP for, so we don't check for v6 bip conflict
+	ip = lastIPInSubnet(ip, mask)
+
+	return ip, mask
+}
+
 type DockerContainersDiff struct {
 	Added   []dockertypes.ContainerSummaryMin
 	Removed []dockertypes.ContainerSummaryMin
+}
+
+func prefixToMask(prefix netip.Prefix) net.IPMask {
+	nBits := prefix.Bits()
+	mask := make(net.IPMask, len(prefix.Addr().AsSlice()))
+	for i := 0; i < len(mask); i++ {
+		if nBits >= 8 {
+			mask[i] = 0xff
+			nBits -= 8
+		} else if nBits > 0 {
+			mask[i] = byte(0xff << (8 - nBits))
+			nBits = 0
+		} else {
+			mask[i] = 0
+		}
+	}
+	return mask
+}
+
+// last IP in range
+func lastIPInSubnet(addr net.IP, mask net.IPMask) net.IP {
+	// copy
+	addr = append([]byte(nil), addr...)
+
+	// apply mask
+	for i := range addr {
+		addr[i] |= ^mask[i]
+	}
+
+	// subtract 1 from last octet to avoid broadcast
+	addr[len(addr)-1]--
+
+	return addr
 }
--- vmgr/vnet/host_bridge.go
+++ vmgr/vnet/host_bridge.go
@@ -174,25 +174,14 @@ func (n *Network) AddVlanBridge(config sgtypes.DockerBridgeConfig) (int, error) 
 	}
 
 	if config.IP4Subnet.IsValid() {
-		mask := prefixToMask(config.IP4Subnet)
-		ip := net.IP(config.IP4Subnet.Addr().AsSlice())
-		// last IP - to avoid conflict with containers or gateway
-		ip = lastIPInSubnet(ip, mask)
-		// if it conflicts with the Linux-side host/gateway IP (bip), subtract 1 more from last octet
-		if ip.Equal(config.IP4Gateway.AsSlice()) {
-			ip[len(ip)-1]--
-		}
+		ip, mask := config.HostIP4()
 		vmnetConfig.Ip4Address = ip.String()
 		vmnetConfig.Ip4Mask = net.IP(mask).String()
 	}
 
 	if config.IP6Subnet.IsValid() {
-		mask := prefixToMask(config.IP6Subnet)
-		ip := net.IP(config.IP6Subnet.Addr().AsSlice())
-		// last IP - to avoid conflict with containers or gateway
-		// this has basically no chance of conflicting with a user-selected or SLAAC IP,
-		// and Docker doesn't provide gateway IP for, so we don't check for v6 bip conflict
-		ip = lastIPInSubnet(ip, mask)
+		// macOS mask is always /64. we check this on docker side too
+		ip, _ := config.HostIP6()
 		vmnetConfig.Ip6Address = ip.String()
 	}
 
@@ -273,39 +262,6 @@ func deriveBridgeConfigUuid(config sgtypes.DockerBridgeConfig) string {
 	// uuid format
 	uuidBytes := h[:16]
 	return fmt.Sprintf("%08x-%04x-%04x-%04x-%012x", uuidBytes[0:4], uuidBytes[4:6], uuidBytes[6:8], uuidBytes[8:10], uuidBytes[10:16])
-}
-
-func prefixToMask(prefix netip.Prefix) net.IPMask {
-	nBits := prefix.Bits()
-	mask := make(net.IPMask, len(prefix.Addr().AsSlice()))
-	for i := 0; i < len(mask); i++ {
-		if nBits >= 8 {
-			mask[i] = 0xff
-			nBits -= 8
-		} else if nBits > 0 {
-			mask[i] = byte(0xff << (8 - nBits))
-			nBits = 0
-		} else {
-			mask[i] = 0
-		}
-	}
-	return mask
-}
-
-// last IP in range
-func lastIPInSubnet(addr net.IP, mask net.IPMask) net.IP {
-	// copy
-	addr = append([]byte(nil), addr...)
-
-	// apply mask
-	for i := range addr {
-		addr[i] |= ^mask[i]
-	}
-
-	// subtract 1 from last octet to avoid broadcast
-	addr[len(addr)-1]--
-
-	return addr
 }
 
 func (n *Network) CreateSconMachineHostBridge() error {
