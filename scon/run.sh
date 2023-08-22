e2e411dd7181198539655f36569f65de0cd7f2c4
Danny Lin <danny@kdrag0n.dev>
2023-08-22 05:04:59 -0700

    wait for scon exit to fix ports breaking in dev

--- scon/run.sh
+++ scon/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-killall -9 scon
+# wait for scon to exit cleanly
+# otherwise forward state gets messed up
+killall scon
+while killall -0 scon; do
+    sleep 0.1
+done
+
 exec ./scon "$@"
