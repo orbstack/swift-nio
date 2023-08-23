3afeff53e778980c5d71ca12a626bcab7d1dac44
Danny Lin <danny@kdrag0n.dev>
2023-08-23 04:47:49 -0700

    add masquerade to fix kind ipv6 outgoing
    kind creates docker bridge net with v6, so by RFC, it prefers to use v6
    to connect to other ULAs like host.docker.internal
    
    but there's no masquerade so it can't be reached
    
    fix: masquerade all IPv6 from ULA source, except if it came from the
    machine v6 subnet (which probably should never happen, actually? but
    let's be safe)
    
    https://github.com/orbstack/orbstack/issues/549
    
    also update ipv4 netconf to match k8s reserved

--- rootfs/utils/docker-init.sh
+++ rootfs/utils/docker-init.sh
@@ -11,6 +11,14 @@ mount --make-rshared /
 mkdir /run/host-services
 ln -sf /opt/orbstack-guest/run/host-ssh-agent.sock /run/host-services/ssh-auth.sock
 
-ip6tables -t nat -A POSTROUTING -s fd07:b51a:cc66:1::/64 -o eth0 -j MASQUERADE
-iptables -t nat -A PREROUTING -s 192.168.215.0/24,192.168.228.0/24,192.168.247.0/24,192.168.207.0/24,192.168.167.0/24,192.168.107.0/24,192.168.237.0/24,192.168.148.0/24,192.168.214.0/24,192.168.165.0/24,192.168.227.0/24,192.168.181.0/24,192.168.158.0/24,192.168.117.0/24,192.168.155.0/24,192.168.194.0/24 -d 172.17.0.1 -j DNAT --to-destination 198.19.249.2
+iptables -t nat -N ORB-PREROUTING
+iptables -t nat -A PREROUTING -j ORB-PREROUTING
+iptables -t nat -A ORB-PREROUTING -s 192.168.215.0/24,192.168.228.0/24,192.168.247.0/24,192.168.207.0/24,192.168.167.0/24,192.168.107.0/24,192.168.237.0/24,192.168.148.0/24,192.168.214.0/24,192.168.165.0/24,192.168.227.0/24,192.168.181.0/24,192.168.158.0/24,192.168.117.0/24,192.168.155.0/24 -d 172.17.0.1 -j DNAT --to-destination 198.19.249.2
+
+ip6tables -t nat -N ORB-POSTROUTING
+ip6tables -t nat -A POSTROUTING -j ORB-POSTROUTING
+ip6tables -t nat -N ORB-POSTROUTING-S1
+ip6tables -t nat -A ORB-POSTROUTING-S1 '!' -s fd07:b51a:cc66::/64 -j MASQUERADE
+ip6tables -t nat -A ORB-POSTROUTING -s fc00::/7 -o eth0 -j ORB-POSTROUTING-S1
+
 exec /opt/orbstack-guest/simplevisor
