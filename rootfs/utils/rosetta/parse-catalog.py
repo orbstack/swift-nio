0a2f29eef648b71a78374ad7bc20b5a339e4ae30
Danny Lin <danny@kdrag0n.dev>
2023-08-22 23:27:43 -0700

    rosetta: bump to sonoma b6
    no real change

--- rootfs/utils/rosetta/parse-catalog.py
+++ rootfs/utils/rosetta/parse-catalog.py
@@ -9,8 +9,8 @@ src_version_prefixes = [
     # 14.x
     '23',
 ]
-# macOS 14 Developer Beta 4
-target_version = '23A5312d'
+# macOS 14 beta 6
+target_version = '23A5328b'
 
 for product in catalog['Products'].values():
     mac_build_version = product['ExtendedMetaInfo']['BuildVersion']
