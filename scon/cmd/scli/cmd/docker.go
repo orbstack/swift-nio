1e40b279e2f7d35c37e84b389e9d1acd11c19903
Danny Lin <danny@kdrag0n.dev>
2023-08-24 03:15:36 -0700

    update migrate help

--- scon/cmd/scli/cmd/docker.go
+++ scon/cmd/scli/cmd/docker.go
@@ -24,22 +24,22 @@ This includes building and running Docker containers, as well as using Docker Co
 	RunE: func(cmd *cobra.Command, args []string) error {
 		scli.EnsureSconVMWithSpinner()
 
-		fmt.Print(strings.Replace(`The "orb" and "orbctl" commands are for managing OrbStack and full Linux machines.
+		fmt.Print(strings.Replace(`The "orb" and "orbctl" commands are for managing OrbStack and its Linux machines.
 
-To build and run Docker containers and manage anything related to Docker, use the "docker" command directly from macOS:
+To build and run containers and manage anything related to Docker, use the "docker" command directly from macOS:
     docker
 There is no need to use Linux machines if you only want to use Docker.
 
 For example, to start an example container:
     docker run -p 80:80 docker/getting-started
-Then visit http://localhost in your browser.
+Then visit http://localhost.
 
-Docker Compose and buildx are also included:
+Compose and buildx are also included:
     docker compose
     docker buildx
 
 To migrate containers, volumes, and images from Docker Desktop:
-    orb docker migrate
+    orb migrate docker
 `, "<HOST>", appid.AppName, -1))
 
 		return nil
