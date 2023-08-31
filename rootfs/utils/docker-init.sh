0cdc992ff240f6a5c8ad11998416ed0bf6492e0a
Danny Lin <danny@kdrag0n.dev>
2023-08-31 07:39:50 -0700

    masquerade to fix src IP for docker ipv4 iptables forwards
    note: docker doesnt do iptables for v6
    
    repro:
    docker run -it --rm -p 80:80 nginx
    curl -4 localhost
    curl -6 localhost
    
    check source ips in log

--- rootfs/utils/docker-init.sh
+++ rootfs/utils/docker-init.sh
@@ -14,11 +14,15 @@ ln -sf /opt/orbstack-guest/run/host-ssh-agent.sock /run/host-services/ssh-auth.s
 iptables -t nat -N ORB-PREROUTING
 iptables -t nat -A PREROUTING -j ORB-PREROUTING
 iptables -t nat -A ORB-PREROUTING -s 192.168.215.0/24,192.168.228.0/24,192.168.247.0/24,192.168.207.0/24,192.168.167.0/24,192.168.107.0/24,192.168.237.0/24,192.168.148.0/24,192.168.214.0/24,192.168.165.0/24,192.168.227.0/24,192.168.181.0/24,192.168.158.0/24,192.168.117.0/24,192.168.155.0/24 -d 172.17.0.1 -j DNAT --to-destination 198.19.249.2
+iptables -t nat -N ORB-POSTROUTING
+iptables -t nat -A POSTROUTING -j ORB-POSTROUTING
+iptables -t nat -A ORB-POSTROUTING -s 198.19.248.1/32 '!' -d 127.0.0.0/8 '!' -o eth0 -j MASQUERADE
 
 ip6tables -t nat -N ORB-POSTROUTING
 ip6tables -t nat -A POSTROUTING -j ORB-POSTROUTING
 ip6tables -t nat -N ORB-POSTROUTING-S1
 ip6tables -t nat -A ORB-POSTROUTING-S1 '!' -s fd07:b51a:cc66::/64 -j MASQUERADE
 ip6tables -t nat -A ORB-POSTROUTING -s fc00::/7 -o eth0 -j ORB-POSTROUTING-S1
+ip6tables -t nat -A ORB-POSTROUTING -s fd07:b51a:cc66:f0::1/128 '!' -d ::1/128 '!' -o eth0 -j MASQUERADE
 
 exec /opt/orbstack-guest/simplevisor
