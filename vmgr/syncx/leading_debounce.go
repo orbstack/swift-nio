f747ce39df67ac02649ec5d11c5d60f5a633e915
Danny Lin <danny@kdrag0n.dev>
2023-08-31 07:11:45 -0700

    add leading debounce to scon
    TODO merge packages

---
+++ vmgr/syncx/leading_debounce.go
@@ -0,0 +1,56 @@
+package syncx
+
+import (
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+// leading debounce: call immediately, then ignore calls for duration. DO not reset the timer
+type LeadingFuncDebounce struct {
+	// avoid blocking callers if func is slow
+	mu       sync.Mutex
+	funcMu   sync.Mutex
+	fn       func()
+	duration time.Duration
+	timer    *time.Timer
+	pending  atomic.Bool
+}
+
+func NewLeadingFuncDebounce(fn func(), duration time.Duration) *LeadingFuncDebounce {
+	return &LeadingFuncDebounce{
+		fn:       fn,
+		duration: duration,
+	}
+}
+
+func (d *LeadingFuncDebounce) Trigger() {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.timer == nil {
+		go d.invoke()
+		d.timer = time.AfterFunc(d.duration, func() {
+			d.mu.Lock()
+			// reset
+			d.timer = nil
+			d.mu.Unlock()
+			// call if needed
+			if d.pending.Swap(false) {
+				d.invoke()
+			}
+		})
+	} else {
+		d.pending.Store(true)
+	}
+}
+
+func (d *LeadingFuncDebounce) invoke() {
+	d.funcMu.Lock()
+	defer d.funcMu.Unlock()
+	d.fn()
+}
+
+func (d *LeadingFuncDebounce) CallNow() {
+	go d.invoke()
+}
