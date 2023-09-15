9fee38850b1454f6a2ba6021457cc61e45f526ee
Danny Lin <danny@kdrag0n.dev>
2023-09-15 10:22:10 -0700

    temporarily revert EWMA

--- vmgr/vnet/tcpfwd/pump_copy.go
+++ vmgr/vnet/tcpfwd/pump_copy.go
@@ -2,8 +2,6 @@ package tcpfwd
 
 import (
 	"io"
-
-	"github.com/orbstack/macvirt/vmgr/util/ewma"
 )
 
 const (
@@ -12,8 +10,6 @@ const (
 	defaultBufferSize = 65536
 
 	zeroCopyGvBufferSize = 512 * 1024
-
-	ewmaWeight = 1.0 / 128.0
 )
 
 // io.CopyBuffer with ewma
@@ -31,9 +27,6 @@ func pumpCopyBuffer(dst io.Writer, src io.Reader, buf []byte) (written int64, er
 	if buf == nil {
 		buf = make([]byte, defaultBufferSize)
 	}
-	avg := ewma.NewF32(float32(len(buf)), ewmaWeight)
-	bufThresLo := uint64(len(buf) / 4)
-	bufThresHi := bufThresLo * 3
 	for {
 		nr, er := src.Read(buf)
 		if nr > 0 {
@@ -52,27 +45,6 @@ func pumpCopyBuffer(dst io.Writer, src io.Reader, buf []byte) (written int64, er
 			if nr != nw {
 				err = io.ErrShortWrite
 				break
-			}
-
-			newAvg := uint64(avg.Update(float32(nr)))
-			if newAvg > bufThresHi && len(buf) < maxBufferSize {
-				// next pow2 - move up 1
-				targetSize := 1 << ewma.CeilILog2(uint(len(buf)+1))
-				targetSize = min(maxBufferSize, targetSize)
-				targetSize = max(minBufferSize, targetSize)
-
-				buf = make([]byte, targetSize)
-				bufThresLo = uint64(len(buf) / 4)
-				bufThresHi = uint64(len(buf) * 3 / 4)
-			} else if newAvg < bufThresLo && len(buf) > minBufferSize {
-				// next pow2 - move down 1
-				targetSize := 1 << ewma.FloorILog2(uint(len(buf)-1))
-				targetSize = min(maxBufferSize, targetSize)
-				targetSize = max(minBufferSize, targetSize)
-
-				buf = make([]byte, targetSize)
-				bufThresLo = uint64(len(buf) / 4)
-				bufThresHi = uint64(len(buf) * 3 / 4)
 			}
 		}
 		if er != nil {
