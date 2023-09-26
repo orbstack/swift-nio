136a437a8262a62013bfe2c63607baf95c8c9d5e
Danny Lin <danny@kdrag0n.dev>
2023-09-26 01:54:21 -0700

    ewma: only scale up, not down
    matches linux tcp_moderate_rcvbuf

--- vmgr/vnet/tcpfwd/pump_copy.go
+++ vmgr/vnet/tcpfwd/pump_copy.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	minBufferSize     = 4096
-	maxBufferSize     = 2 * 1024 * 1024
+	maxBufferSize     = 2 * 1024 * 1024 // 2 MiB
 	defaultBufferSize = 65536
 
 	zeroCopyGvBufferSize = 512 * 1024
@@ -29,11 +29,11 @@ func pumpCopyBuffer(dst io.Writer, src io.Reader, buf []byte) (written int64, er
 		return rt.ReadFrom(src)
 	}
 	if buf == nil {
+		// TODO use gvisor view pooling
 		buf = make([]byte, defaultBufferSize)
 	}
 	avg := ewma.NewF32(float32(len(buf)), ewmaWeight)
-	bufThresLo := uint64(len(buf) / 4)
-	bufThresHi := bufThresLo * 3
+	bufThresHi := uint64(len(buf) * 3 / 4)
 	for {
 		nr, er := src.Read(buf)
 		if nr > 0 {
@@ -54,6 +54,10 @@ func pumpCopyBuffer(dst io.Writer, src io.Reader, buf []byte) (written int64, er
 				break
 			}
 
+			// only scale up, not down.
+			// - prevents oscillating
+			// - no need for ratelimit, so it scales up fast
+			// TODO consider jumping by 2 powers of 2
 			newAvg := uint64(avg.Update(float32(nr)))
 			if newAvg > bufThresHi && len(buf) < maxBufferSize {
 				// next pow2 - move up 1
@@ -62,16 +66,6 @@ func pumpCopyBuffer(dst io.Writer, src io.Reader, buf []byte) (written int64, er
 				targetSize = max(minBufferSize, targetSize)
 
 				buf = make([]byte, targetSize)
-				bufThresLo = uint64(len(buf) / 4)
-				bufThresHi = uint64(len(buf) * 3 / 4)
-			} else if newAvg < bufThresLo && len(buf) > minBufferSize {
-				// next pow2 - move down 1
-				targetSize := 1 << ewma.FloorILog2(uint(len(buf)-1))
-				targetSize = min(maxBufferSize, targetSize)
-				targetSize = max(minBufferSize, targetSize)
-
-				buf = make([]byte, targetSize)
-				bufThresLo = uint64(len(buf) / 4)
 				bufThresHi = uint64(len(buf) * 3 / 4)
 			}
 		}
