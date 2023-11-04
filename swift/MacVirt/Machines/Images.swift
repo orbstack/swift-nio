2123186db0dccf996069c8eac694f1dd1bf2234b
Danny Lin <danny@kdrag0n.dev>
2023-11-03 21:30:56 -0700

    gui: images: add debian 13 trixie as testing

--- swift/MacVirt/Machines/Images.swift
+++ swift/MacVirt/Machines/Images.swift
@@ -84,7 +84,7 @@ enum Distro: String, CaseIterable {
         case .alpine:   return [v("edge"), v("3.15"), v("3.16"), v("3.17"), v("3.18")]
         case .arch:     return [v("current", as: "Latest")]
         case .centos:   return [/*v("7"),*/ v("8-Stream", as: "8 (Stream)"), v("9-Stream", as: "9 (Stream)")]
-        case .debian:   return [v("buster", as: "10 (Buster)"), v("bullseye", as: "11 (Bullseye)"), v("sid", as: "Sid (unstable)"), v("bookworm", as: "12 (Bookworm)")]
+        case .debian:   return [v("buster", as: "10 (Buster)"), v("bullseye", as: "11 (Bullseye)"), v("trixie", as: "13 (Trixie, testing)"), v("sid", as: "Sid (unstable)"), v("bookworm", as: "12 (Bookworm)")]
         case .devuan: return [v("beowulf", as: "Beowulf"), v("chimaera", as: "Chimaera"), v("daedalus", as: "Daedalus")]
         case .fedora:   return [v("37"), v("38")]
         case .gentoo:   return [v("current", as: "Latest")]
