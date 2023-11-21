orbstack/swift-nio

commit 56477e373550ed0a0a86ed69cb1007d88653de67

attempt recover from tcp close panic
panic: length < 0
	panic: length < 0

goroutine 77 [running]:
gvisor.dev/gvisor/pkg/tcpip/stack.PacketData.CapLength(...)
	gvisor.dev/gvisor@v0.0.0-20230918234652-8a7617aed21c/pkg/tcpip/stack/packet_buffer.go:593
gvisor.dev/gvisor/pkg/tcpip/transport/tcp.(*sender).splitSeg(0x14000263340, 0x1400171c100, 0x814a72092fc7f518)
	gvisor.dev/gvisor@v0.0.0-20230918234652-8a7617aed21c/pkg/tcpip/transport/tcp/snd.go:613 +0x1bc
gvisor.dev/gvisor/pkg/tcpip/transport/tcp.(*sender).maybeSendSegment(0x14000263340, 0x1400171c100, 0x0?, 0x214600?)
	gvisor.dev/gvisor@v0.0.0-20230918234652-8a7617aed21c/pkg/tcpip/transport/tcp/snd.go:872 +0x1c4
gvisor.dev/gvisor/pkg/tcpip/transport/tcp.(*sender).sendData(0x14000263340)
	gvisor.dev/gvisor@v0.0.0-20230918234652-8a7617aed21c/pkg/tcpip/transport/tcp/snd.go:982 +0x1d0
gvisor.dev/gvisor/pkg/tcpip/transport/tcp.(*endpoint).sendData(0xb011?, 0x1f813c6?)
	gvisor.dev/gvisor@v0.0.0-20230918234652-8a7617aed21c/pkg/tcpip/transport/tcp/connect.go:1009 +0x40
gvisor.dev/gvisor/pkg/tcpip/transport/tcp.(*endpoint).shutdownLocked(0x1400002c000, 0x106e58420?)
	gvisor.dev/gvisor@v0.0.0-20230918234652-8a7617aed21c/pkg/tcpip/transport/tcp/endpoint.go:2567 +0x490
gvisor.dev/gvisor/pkg/tcpip/transport/tcp.(*endpoint).closeLocked(0x1400002c000)
	gvisor.dev/gvisor@v0.0.0-20230918234652-8a7617aed21c/pkg/tcpip/transport/tcp/endpoint.go:1089 +0xa0
gvisor.dev/gvisor/pkg/tcpip/transport/tcp.(*endpoint).Close(0x1400002c000)
	gvisor.dev/gvisor@v0.0.0-20230918234652-8a7617aed21c/pkg/tcpip/transport/tcp/endpoint.go:1059 +0x30
github.com/orbstack/macvirt/vmgr/vnet/gonet.(*TCPConn).Close(0x1400002c000?)
	github.com/orbstack/macvirt/vmgr/vnet/gonet/gonet.go:447 +0x28
panic({0x105baae00?, 0x105f9c2f0?})
	runtime/panic.go:914 +0x218
gvisor.dev/gvisor/pkg/tcpip/stack.PacketData.CapLength(...)
	gvisor.dev/gvisor@v0.0.0-20230918234652-8a7617aed21c/pkg/tcpip/stack/packet_buffer.go:593

vmgr/vnet/gonet/gonet.go
@@ -442,9 +442,40 @@ func (c *TCPConn) Write(b []byte) (int, error) {
 	return nbytes, nil
 }
 
+func (c *TCPConn) tryClose() (err error) {
+	defer func() {
+		if err := recover(); err != nil {
+			err = fmt.Errorf("gtcp: close panic: %v", err)
+		}
+	}()
+
+	c.ep.Close()
+	return
+}
+
+func (c *TCPConn) tryAbort() (err error) {
+	defer func() {
+		if err := recover(); err != nil {
+			err = fmt.Errorf("gtcp: abort panic: %v", err)
+		}
+	}()
+
+	c.ep.Abort()
+	return
+}
+
 // Close implements net.Conn.Close.
 func (c *TCPConn) Close() error {
-	c.ep.Close()
+	err := c.tryClose()
+	if err != nil {
+		err2 := c.tryAbort()
+		if err2 != nil {
+			return fmt.Errorf("gtcp: close: %v (abort: %v)", err, err2)
+		}
+
+		return err
+	}
+
 	return nil
 }
 

vmgr/vnet/tcpfwd/hosttcp.go
@@ -172,7 +172,7 @@ func (f *TcpHostForward) handleConn(conn net.Conn) {
 		logrus.WithError(err).WithField("addr", connectAddr).Error("host-tcp forward: dial failed")
 		return
 	}
-	defer tryBestCleanup(virtConn)
+	defer virtConn.Close()
 
 	// other port doesn't matter, only service does (client port should be ephemeral)
 	err = setExtNodelay(conn.(*net.TCPConn), 0)

vmgr/vnet/tcpfwd/tcp.go
@@ -3,7 +3,6 @@ package tcpfwd
 import (
 	"context"
 	"errors"
-	"fmt"
 	"net"
 	"strconv"
 	"time"
@@ -28,36 +27,6 @@ const (
 	// set very high for nmap
 	listenBacklog = 65535
 )
-
-func tryClose(conn *gonet.TCPConn) (err error) {
-	defer func() {
-		if err := recover(); err != nil {
-			err = fmt.Errorf("tcpfwd: close panic: %v", err)
-		}
-	}()
-
-	conn.Close()
-	return
-}
-
-func tryAbort(conn *gonet.TCPConn) (err error) {
-	defer func() {
-		if err := recover(); err != nil {
-			err = fmt.Errorf("tcpfwd: abort panic: %v", err)
-		}
-	}()
-
-	conn.Endpoint().Abort()
-	return
-}
-
-func tryBestCleanup(conn *gonet.TCPConn) error {
-	err := tryClose(conn)
-	if err != nil {
-		return err
-	}
-	return tryAbort(conn)
-}
 
 func NewTcpForwarder(s *stack.Stack, icmpMgr *icmpfwd.IcmpFwd, hostNatIP4 tcpip.Address, hostNatIP6 tcpip.Address, bridgeRouteMon *bridge.RouteMon) (*tcp.Forwarder, *ProxyManager) {
 	proxyMgr := newProxyManager(hostNatIP4, hostNatIP6)
@@ -164,9 +133,9 @@ func NewTcpForwarder(s *stack.Stack, icmpMgr *icmpfwd.IcmpFwd, hostNatIP4 tcpip.
 
 		virtConn := gonet.NewTCPConn(&wq, ep)
 		defer func() {
-			err := tryBestCleanup(virtConn)
+			err := virtConn.Close()
 			if err != nil {
-				logrus.Error("tcpfwd: cleanup panic", err)
+				logrus.WithError(err).Error("TCP forward: close failed")
 			}
 		}()
 

commit 17922ed8668e83ac26744335b634583c16c882b8

vnet: always abort TCP connections after closing
commit 56477e373 ("attempt recover from tcp close panic") caused nginx
timeouts after high load / requests

it's because of a subtle logic bug in our OLD impl that actually FIXED
the issue:
always calling tryAbort in tryClose fixed it.

gvisor bug? time-wait issue?

repro:
- run port 3000 http server on macOS
- write nginx.conf
- docker run -it --rm -p 80:80 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf nginx
- hey -z 25s http://localhost/

expected behavior: no timeouts, all 200
actual: timeouts w/o this patch

nginx.conf:
user  nginx;
worker_processes  auto;

error_log  /var/log/nginx/error.log notice;
pid        /var/run/nginx.pid;

events {
    worker_connections  1024;
}

http {
    include       /etc/nginx/mime.types;
    default_type  application/octet-stream;

    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
                      '$status $body_bytes_sent "$http_referer" '
                      '"$http_user_agent" "$http_x_forwarded_for"';

    access_log  /var/log/nginx/access.log  main;

    sendfile        on;
    #tcp_nopush     on;

    keepalive_timeout  65;

    #gzip  on;

    #include /etc/nginx/conf.d/*.conf;

    server {
        # The listen directive serves content based on the port defined
        listen 80; # For IPv4 addresses
        listen [::]:80; # For IPv6 addresses

        # Replace the below if you have a domain name pointed to the server
        server_name default;

        location / {
	    # Specify the proxied server's address
            proxy_pass http://host.docker.internal:3000;
        }
    }
}

vmgr/vnet/gonet/gonet.go
@@ -479,16 +479,11 @@ func (c *TCPConn) tryAbort() (retErr error) {
 
 // Close implements net.Conn.Close.
 func (c *TCPConn) Close() error {
-	err := c.tryClose()
-	if err != nil {
-		err2 := c.tryAbort()
-		if err2 != nil {
-			return fmt.Errorf("gtcp: close: %v (abort: %v)", err, err2)
-		}
-
-		return err
+	err1 := c.tryClose()
+	err2 := c.tryAbort()
+	if err1 != nil || err2 != nil {
+		return fmt.Errorf("gtcp: close failed: %w, abort failed: %w", err1, err2)
 	}
-
 	return nil
 }
 