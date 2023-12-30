dd7dc4192255fd515c64950242728d7d543c3ed6
Danny Lin <danny@kdrag0n.dev>
2023-12-29 19:24:40 -0800

    scon: build with v3 BPF ISA
    bpf2go defaults to v1. this should improve perf slightly

--- scon/bpf/build-bpf.sh
+++ scon/bpf/build-bpf.sh
@@ -4,14 +4,17 @@ set -eufo pipefail
 
 cd "$(dirname "$0")"
 
-go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel bnat src/bnat.c
+# new ISA, kernel 5.1+ (v4 requires 6.6+ and LLVM 18)
+BPF_CFLAGS="-mcpu=v3"
+
+go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -cflags "$BPF_CFLAGS" bnat src/bnat.c
 # strip source line info
 go run ../cmd/btfstrip bnat_bpfel.o
 
-go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel lfwd src/lfwd.c
+go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -cflags "$BPF_CFLAGS" lfwd src/lfwd.c
 # strip source line info
 go run ../cmd/btfstrip lfwd_bpfel.o
 
-go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel pmon src/pmon.c
+go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -cflags "$BPF_CFLAGS" pmon src/pmon.c
 # strip source line info
 go run ../cmd/btfstrip pmon_bpfel.o
