03d068c1960410126fb61ed6f30c9c8aa269a54f
Danny Lin <danny@kdrag0n.dev>
2024-02-24 02:47:19 -0800

    wormhole: fs: remove SCHED_FIFO priority
    this part doesn't build on musl (more struct fields)
    
    and we don't need it for wormholefs

--- wormhole/wormholefs/src/bin/wormholefsd/main.rs
+++ wormhole/wormholefs/src/bin/wormholefsd/main.rs
@@ -12,13 +12,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = std::env::args().collect::<Vec<_>>();
     let client = WormholeFs::new(args[1].as_str(), args[2].as_str(), args[3].as_str())?;
 
-    let param = sched_param {
-        sched_priority: 1,
-    };
-    let ret = unsafe { pthread_setschedparam(pthread_self(), SCHED_FIFO, &param) };
-    if ret != 0 {
-        panic!("failed to set priority");
-    }
-
     client.read_fuse_events()
 }
