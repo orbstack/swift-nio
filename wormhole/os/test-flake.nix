c8b9c1da54ef1a3035b419788039ae02bcc8a903
Danny Lin <danny@kdrag0n.dev>
2024-03-09 19:47:48 -0800

    wormhole: merge test flakes

--- wormhole/os/test-flake.nix
+++ wormhole/os/test-flake.nix
@@ -1,25 +1,15 @@
 {
-#   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            # go 1.20 (specified by overlay)
-            go
-            gotools
-            golangci-lint
-          ];
-        };
-      });
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }: {
+    defaultPackage.aarch64-linux = nixpkgs.legacyPackages.aarch64-linux.buildEnv {
+      name = "wormhole-env";
+      paths = with nixpkgs.legacyPackages.aarch64-linux; [
+        go
+        gotools
+        golangci-lint
+      ];
+      pathsToLink = [ "/" ];
+      #extraOutputsToInstall = [ "man" "doc" ];
     };
+  };
 }
