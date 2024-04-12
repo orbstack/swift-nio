9f04118e3f7a0e3c5d0aba132e1f197e14b2bce6
Danny Lin <danny@kdrag0n.dev>
2024-04-11 17:01:47 -0700

    add k8s build script

---
+++ scripts/build-k8s.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eufo pipefail
+cd "$(dirname "$0")"
+
+export GOPRIVATE='github.com/orbstack/*-macvirt'
+BRANCH=orbstack/1.27
+
+orb start
+
+echo "Cloning repos"
+
+cd ../vendor
+[[ ! -d "k3s" ]] && git clone -b $BRANCH git@github.com:orbstack/k3s || :
+
+pushd k3s
+pushd forks
+[[ ! -d "kubernetes" ]] && git clone -b $BRANCH git@github.com:orbstack/kubernetes || :
+[[ ! -d "cri-dockerd" ]] && git clone -b $BRANCH git@github.com:orbstack/cri-dockerd || :
+popd
+
+echo "Building"
+make
+
+cp -f bin/k3s ../../rootfs/k8s/k3s-arm64
