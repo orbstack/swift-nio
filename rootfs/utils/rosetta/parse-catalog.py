ec367d7bfda14432c88d1ba53fc615bbd300398a
Danny Lin <danny@kdrag0n.dev>
2024-05-03 03:16:33 -0700

    rootfs: update Rosetta to macOS 14.5 beta 4 (23F5074a)
    no change from 14.4.1 (318.6)
    
    27bb27a0fefc4c4def9f64616554d52975bab95e20378a173207da4a0d383398

--- rootfs/utils/rosetta/parse-catalog.py
+++ rootfs/utils/rosetta/parse-catalog.py
@@ -9,8 +9,8 @@ src_version_prefixes = [
     # 14.x
     '23',
 ]
-# macOS 14.4.1
-target_version = '23E224'
+# macOS 14.5 beta 4
+target_version = '23F5074a'
 
 for product in catalog['Products'].values():
     mac_build_version = product['ExtendedMetaInfo']['BuildVersion']
