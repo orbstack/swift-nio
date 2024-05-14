a33a5ed6ba3bc3a60cd8da8e6ee2d2aa8d8c3cb4
Danny Lin <danny@kdrag0n.dev>
2024-05-14 16:06:13 -0700

    virtue: hvf: don't inherit VM memory mappings on fork
    large mappings (e.g. 64G) cause fork to take ~1 sec
    
    we tried to use posix_spawn to fix this in vmgr by avoiding fork, but
    there's still one remaining usage of exec.Command in vmgr due to lack of
    setctty support. this is an easier, guaranteed fix that we should keep
    even after a full migration to posix_spawn.

--- src/hvf/src/lib.rs
+++ src/hvf/src/lib.rs
@@ -9,6 +9,24 @@ mod aarch64;
 #[cfg(target_arch = "aarch64")]
 pub use aarch64::*;
 
+// TODO: unify all our libsystem externs into one package
+mod sys {
+    extern "C" {
+        pub fn minherit(addr: *const libc::c_void, len: usize, inherit: libc::c_int)
+            -> libc::c_int;
+    }
+
+    pub const VM_INHERIT_NONE: libc::c_int = 2;
+}
+
+fn minherit(addr: *const libc::c_void, len: usize, inherit: libc::c_int) -> nix::Result<()> {
+    let ret = unsafe { sys::minherit(addr, len, inherit) };
+    if ret == -1 {
+        return Err(nix::Error::last());
+    }
+    Ok(())
+}
+
 // on macOS, use the HVF API to allocate guest memory. it seems to use mach APIs
 // standard mmap causes 2x overaccounting in Activity Monitor's "Memory" tab
 pub fn allocate_guest_memory(ranges: &[(GuestAddress, usize)]) -> anyhow::Result<GuestMemoryMmap> {
@@ -16,7 +34,14 @@ pub fn allocate_guest_memory(ranges: &[(GuestAddress, usize)]) -> anyhow::Result
         .iter()
         .map(|(guest_base, size)| {
             let host_addr = unsafe { vm_allocate(*size) }
-                .map_err(|_| anyhow::anyhow!("failed to allocate host memory"))?;
+                .map_err(|e| anyhow::anyhow!("failed to allocate host memory: {}", e))?;
+
+            // with large amounts of vm_allocated space (e.g. ~64 GiB) in the process, fork takes a very long time (~1 sec)
+            // disable inheritance to fix this
+            // note: these are mach_vm_map regions so we're relying on an implementation detail, but it should be OK because ultimately everything (even mmap) is VM_ALLOCATE on macOS
+            // minherit also does the same thing as mach_vm_inherit in XNU
+            minherit(host_addr, *size, sys::VM_INHERIT_NONE)
+                .map_err(|e| anyhow::anyhow!("failed to disable inheritance: {}", e))?;
 
             let region = unsafe {
                 MmapRegion::build_raw(
@@ -25,11 +50,11 @@ pub fn allocate_guest_memory(ranges: &[(GuestAddress, usize)]) -> anyhow::Result
                     libc::PROT_READ | libc::PROT_WRITE,
                     libc::MAP_ANONYMOUS | libc::MAP_NORESERVE | libc::MAP_PRIVATE,
                 )
-                .map_err(|_| anyhow::anyhow!("failed to create mmap region"))?
+                .map_err(|e| anyhow::anyhow!("failed to create mmap region: {}", e))?
             };
 
             GuestRegionMmap::new(region, *guest_base)
-                .map_err(|_| anyhow::anyhow!("failed to create guest memory region"))
+                .map_err(|e| anyhow::anyhow!("failed to create guest memory region: {}", e))
         })
         .collect::<anyhow::Result<Vec<_>>>()?;
 
