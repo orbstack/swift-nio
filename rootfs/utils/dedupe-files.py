fae64205988524e0a9488ce07dcd33096b8072b9
Danny Lin <danny@kdrag0n.dev>
2024-07-23 06:12:00 -0700

    rootfs: dedupe files by hard linking
    this gets arm64-release down from 367M to 357M. almost just as effective
    as erofs dedupe, but *much* faster
    
    we could make this release-only but it seems fast enough that it doesn't
    matter
    
    * we should technically be checking that uid/gid/mode are the same, but
    it shouldn't really matter for anything in the rootfs. one precaution
    here is to preserve the */nix/* metadata, as nix cares more than alpine
    
    total app size: 422M -> 405M (arm64 dmg), along with 16k pclusters

---
+++ rootfs/utils/dedupe-files.py
@@ -0,0 +1,26 @@
+import collections
+import os
+import sys
+
+hashes = collections.defaultdict(list)
+
+with open(sys.argv[1], 'r') as f:
+    for l in f.read().splitlines():
+        hash, path = l.split(maxsplit=2)
+        hashes[hash].append(path)
+
+for hash, paths in hashes.items():
+    if len(paths) > 1:
+        # prefer uid/gid/mode from */nix/* copy, if any
+        for src_path in paths:
+            if '/nix/' in src_path:
+                break
+        else:
+            src_path = paths[0]
+
+        # delete the rest, and make hard links
+        for p in paths:
+            if p == src_path:
+                continue
+            os.unlink(p)
+            os.link(src_path, p)
