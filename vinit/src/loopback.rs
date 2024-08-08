84087580dbccbaf20a3ca7ab1c21bf6c41b472af
Danny Lin <danny@kdrag0n.dev>
2024-08-08 02:57:29 -0700

    vinit: fix clippy lint

--- vinit/src/loopback.rs
+++ vinit/src/loopback.rs
@@ -6,8 +6,8 @@ const LOOP_CLR_FD: u64 = 0x4C01;
 
 pub fn clear_fd(fd: RawFd) -> std::io::Result<()> {
     let res = unsafe {
-        // cmd type is different on glibc and musl???
-        ioctl(fd, LOOP_CLR_FD.try_into().unwrap(), 0)
+        // cmd type is different on glibc and musl
+        ioctl(fd, LOOP_CLR_FD as _, 0)
     };
     if res != 0 {
         return Err(std::io::Error::last_os_error());
