533f6b2b169844940eb70e07f346a27a70480a7e
Danny Lin <danny@kdrag0n.dev>
2024-08-20 10:04:35 -0700

    scripts/update-appcast: automatically fix release notes URL for canary

--- scripts/update-appcast.py
+++ scripts/update-appcast.py
@@ -11,9 +11,13 @@ namespace = {'sparkle': 'http://www.andymatuschak.org/xml-namespaces/sparkle'}
 
 # Hard-coded values
 description_text = ""
-release_notes_link_text = "https://cdn-updates.orbstack.dev/release-notes.html"
+release_notes_url_stable = "https://cdn-updates.orbstack.dev/release-notes.html"
+release_notes_url_canary = "https://cdn-updates.orbstack.dev/release-notes.canary.html"
 
 for item in root.findall('.//item'):
+    # find version
+    channel = item.find('sparkle:channel', namespace).text
+
     # Check if the item already has these tags, if not add them
     if item.find('description') is None:
         description = ET.SubElement(item, 'description')
@@ -21,7 +25,10 @@ for item in root.findall('.//item'):
 
     if item.find('sparkle:releaseNotesLink', namespace) is None:
         release_notes_link = ET.SubElement(item, '{http://www.andymatuschak.org/xml-namespaces/sparkle}releaseNotesLink')
-        release_notes_link.text = release_notes_link_text
+        if channel == 'canary':
+            release_notes_link.text = release_notes_url_canary
+        else:
+            release_notes_link.text = release_notes_url_stable
 
 ET.register_namespace('sparkle', "http://www.andymatuschak.org/xml-namespaces/sparkle")
 
