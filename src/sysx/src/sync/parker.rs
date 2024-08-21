8ba2554824570eccc08ce053a285afcb6c787d63
Danny Lin <danny@kdrag0n.dev>
2024-08-20 23:50:35 -0700

    virtue: sysx: parker: use Mach semaphores directly
    this is not really a private API. avoid the unnecessary dispatch
    semaphore layer and redundant mach_absolute_time calls
    
    Mach semaphores are still the fastest parking API because
    os_sync_wait_on_address = ulock_wait2, and ulock_wait2 calls ull_get()
    in the kernel, which zallocs a new ull struct that gets freed on wake.

--- src/sysx/src/sync/parker.rs
+++ src/sysx/src/sync/parker.rs
@@ -3,29 +3,28 @@ use std::{
     time::Duration,
 };
 
-#[allow(non_camel_case_types)]
-type dispatch_semaphore_t = *mut std::ffi::c_void;
-#[allow(non_camel_case_types)]
-type dispatch_time_t = u64;
-
-const DISPATCH_TIME_NOW: dispatch_time_t = 0;
-const DISPATCH_TIME_FOREVER: dispatch_time_t = !0;
+use mach2::{
+    clock_types::mach_timespec_t,
+    kern_return::{kern_return_t, KERN_ABORTED, KERN_OPERATION_TIMED_OUT, KERN_SUCCESS},
+    mach_types::{semaphore_t, task_t},
+    semaphore::semaphore_create,
+    sync_policy::SYNC_POLICY_FIFO,
+    traps::mach_task_self,
+};
 
 const EMPTY: i8 = 0;
 const NOTIFIED: i8 = 1;
 const PARKED: i8 = -1;
 
-// Contained in libSystem.dylib, which is linked by default.
-// TODO: Bindings?
 extern "C" {
-    fn dispatch_time(when: dispatch_time_t, delta: i64) -> dispatch_time_t;
-    fn dispatch_semaphore_create(val: isize) -> dispatch_semaphore_t;
-    fn dispatch_semaphore_wait(dsema: dispatch_semaphore_t, timeout: dispatch_time_t) -> isize;
-    fn dispatch_semaphore_signal(dsema: dispatch_semaphore_t) -> isize;
-    fn dispatch_release(object: *mut std::ffi::c_void);
+    // mach2 bindings are wrong for these
+    fn semaphore_wait(semaphore: semaphore_t) -> kern_return_t;
+    fn semaphore_timedwait(semaphore: semaphore_t, wait_time: mach_timespec_t) -> kern_return_t;
+    fn semaphore_signal(semaphore: semaphore_t) -> kern_return_t;
+    fn semaphore_destroy(task: task_t, semaphore: semaphore_t) -> kern_return_t;
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum ParkResult {
     Unparked,
     TimedOut,
@@ -33,7 +32,7 @@ pub enum ParkResult {
 
 #[derive(Debug)]
 pub struct Parker {
-    semaphore: dispatch_semaphore_t,
+    semaphore: semaphore_t,
     state: AtomicI8,
 }
 
@@ -42,8 +41,10 @@ unsafe impl Sync for Parker {}
 
 impl Default for Parker {
     fn default() -> Self {
-        let semaphore = unsafe { dispatch_semaphore_create(0) };
-        assert!(!semaphore.is_null(), "failed to create Parker");
+        let mut semaphore: semaphore_t = 0;
+        let ret =
+            unsafe { semaphore_create(mach_task_self(), &mut semaphore, SYNC_POLICY_FIFO, 0) };
+        assert_eq!(ret, KERN_SUCCESS, "failed to create Parker");
 
         Self {
             semaphore,
@@ -53,12 +54,46 @@ impl Default for Parker {
 }
 
 impl Parker {
+    // specialize for timed vs. untimed
+    #[inline]
+    fn wait(&self, timeout: Option<Duration>) -> ParkResult {
+        loop {
+            let ret = match timeout {
+                Some(timeout) => {
+                    let seconds = timeout.as_secs();
+                    let nanos = timeout.subsec_nanos();
+                    unsafe {
+                        semaphore_timedwait(
+                            self.semaphore,
+                            mach_timespec_t {
+                                tv_sec: seconds as u32,
+                                tv_nsec: nanos as i32,
+                            },
+                        )
+                    }
+                }
+                None => unsafe { semaphore_wait(self.semaphore) },
+            };
+            match ret {
+                // woke up due to semaphore_signal
+                KERN_SUCCESS => return ParkResult::Unparked,
+                // interrupted by signal or thread_abort
+                KERN_ABORTED => continue,
+                // timed out
+                KERN_OPERATION_TIMED_OUT => return ParkResult::TimedOut,
+                // KERN_TERMINATED = semaphore destroyed (should never happen)
+                // others = unexpected
+                _ => panic!("semaphore_wait failed: {}", ret),
+            }
+        }
+    }
+
     pub fn park(&self) {
         if self.state.fetch_sub(1, Acquire) == NOTIFIED {
             return;
         }
 
-        while unsafe { dispatch_semaphore_wait(self.semaphore, DISPATCH_TIME_FOREVER) } != 0 {}
+        while self.wait(None) != ParkResult::Unparked {}
 
         self.state.swap(EMPTY, Acquire);
     }
@@ -68,29 +103,22 @@ impl Parker {
             return ParkResult::Unparked;
         }
 
-        let nanos = timeout.as_nanos().try_into().unwrap_or(i64::MAX);
-        let timeout = unsafe { dispatch_time(DISPATCH_TIME_NOW, nanos) };
-
-        let timeout = unsafe { dispatch_semaphore_wait(self.semaphore, timeout) != 0 };
+        let result = self.wait(Some(timeout));
 
         let state = self.state.swap(EMPTY, Acquire);
-        if state == NOTIFIED && timeout {
+        if state == NOTIFIED && result == ParkResult::TimedOut {
             // If the state was NOTIFIED but semaphore_wait returned without
             // decrementing the count because of a timeout, it means another
             // thread is about to call semaphore_signal. We must wait for that
             // to happen to ensure the semaphore count is reset.
-            while unsafe { dispatch_semaphore_wait(self.semaphore, DISPATCH_TIME_FOREVER) } != 0 {}
+            while self.wait(None) != ParkResult::Unparked {}
             ParkResult::Unparked
         } else {
             // Either a timeout occurred and we reset the state before any thread
             // tried to wake us up, or we were woken up and reset the state,
             // making sure to observe the state change with acquire ordering.
             // Either way, the semaphore counter is now zero again.
-            if timeout {
-                ParkResult::TimedOut
-            } else {
-                ParkResult::Unparked
-            }
+            result
         }
     }
 
@@ -98,7 +126,7 @@ impl Parker {
         let state = self.state.swap(NOTIFIED, Release);
         if state == PARKED {
             unsafe {
-                dispatch_semaphore_signal(self.semaphore);
+                semaphore_signal(self.semaphore);
             }
         }
     }
@@ -106,6 +134,6 @@ impl Parker {
 
 impl Drop for Parker {
     fn drop(&mut self) {
-        unsafe { dispatch_release(self.semaphore) };
+        unsafe { semaphore_destroy(mach_task_self(), self.semaphore) };
     }
 }
