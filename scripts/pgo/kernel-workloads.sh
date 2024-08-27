a48a8212f6dc36492e7e7e7565dc9628d61631b4
Danny Lin <danny@kdrag0n.dev>
2024-08-27 07:42:04 -0700

    scripts: add kernel PGO profiling workloads

---
+++ scripts/pgo/kernel-workloads.sh
@@ -0,0 +1,245 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# NOTE: Docker IPv6 must be enabled!
+
+# Instrumented PGO (as compared to sampling AutoFDO) doesn't care about how *long* this stuff runs for, as long as it hits all the relevant code paths. So a lot of this can be sped up.
+# we also get data from boot, struct page init, erofs loading on boot, etc.
+# TODO: so remove hyperfine from most of these, reduce iperf durations
+
+echo "BEGIN PGO BENCHMARK AT"
+date
+echo
+
+iperf3 -s &
+iperf_server_pid=$!
+
+# all runs with 12 GiB of memory
+
+host_tmp="$(mktemp -d)"
+cd "$host_tmp"
+
+# parallel btrfs ripgrep, uncached
+orb -w "/home/$USER/linux" sudo hyperfine -p 'echo 3 > /proc/sys/vm/drop_caches' 'rg hvc_put; fd -uuu'
+
+# parallel ripgrep, cached
+orb -w "/home/$USER/linux" hyperfine 'rg hvc_put; fd -uuu'
+
+# serial ripgrep, cached
+orb -w "/home/$USER/linux" hyperfine 'rg -j1 hvc_put; fd -j1 -uuu'
+
+# hackbench
+orb -w "/home/$USER" hyperfine 'rt-tests/hackbench -pTl 8000'
+
+# kernel build
+# this also stresses madvise(MADV_DONTNEED) thanks to jemalloc LD_PRELOAD
+# and mprotect contention
+orb -s 'cd ~/linux && source setup.sh && rm -fr o2 && mkdir o2 && cp configs/arm64 o2/.config && marm O=o2'
+
+# pnpm on virtiofs
+cd ~/code/projects/macvirt/exp/fsbench/npm
+# arch linux arm pnpm is broken (incompatible node.js version)
+# run twice: one uncached, one cached
+docker run -i --rm -v $PWD:/data -w /data node sh -c 'rm -fr node_modules && npm install -g pnpm && pnpm i && rm -fr node_modules && pnpm i'
+
+# npm on virtiofs, in docker to stress seccomp
+docker run -i --rm -v $PWD:/data -w /data node sh -c 'rm -fr node_modules && npm i --force'
+
+# npm on overlayfs
+docker run -i --rm -v $PWD:/data node sh -c 'cp /data/package.json . && npm i --force'
+
+# ripgrep on virtiofs
+cd node_modules
+orb sudo hyperfine -p 'echo 3 > /proc/sys/vm/drop_caches' 'rg webp; fd -uuu'
+
+# bpftool on virtiofs
+cd /tmp
+orb sudo hyperfine "bpftool btf dump file /sys/kernel/btf/vmlinux format c > bpftool.txt"
+
+# docker container startup
+hyperfine "docker run -i --rm -v $PWD:/data alpine echo"
+
+# procfs
+orb hyperfine 'ps awux'
+
+# machine restart
+hyperfine 'orb restart ubuntu'
+
+# machine create and delete (squashfs)
+orb delete -f pgo-ubuntu-test || :
+orb create ubuntu pgo-ubuntu-test
+orb delete -f pgo-ubuntu-test
+
+# network in machines
+# iperf, ipv4, TCP, send
+orb iperf3 -c host.orb.internal -4
+# iperf, ipv4, TCP, receive
+orb iperf3 -c host.orb.internal -4 -R
+# iperf, ipv6, TCP, send
+orb iperf3 -c host.orb.internal -6
+# iperf, ipv6, TCP, receive
+orb iperf3 -c host.orb.internal -6 -R
+# iperf, ipv4, UDP, send
+orb iperf3 -c host.orb.internal -4 -u -b 2000M
+# iperf, ipv4, UDP, receive
+orb iperf3 -c host.orb.internal -4 -u -R -b 2000M
+# iperf, ipv6, UDP, send
+orb iperf3 -c host.orb.internal -6 -u -b 2000M
+# iperf, ipv6, UDP, receive
+orb iperf3 -c host.orb.internal -6 -u -R -b 2000M
+
+# network in containers
+docker run -d --rm --name iperf-client-test alpine sleep inf
+docker exec -i iperf-client-test apk add iperf3
+# iperf, ipv4, TCP, send
+docker exec -i iperf-client-test iperf3 -c host.orb.internal -4
+# iperf, ipv4, TCP, receive
+docker exec -i iperf-client-test iperf3 -c host.orb.internal -4 -R
+# iperf, ipv6, TCP, send
+docker exec -i iperf-client-test iperf3 -c host.orb.internal -6
+# iperf, ipv6, TCP, receive
+docker exec -i iperf-client-test iperf3 -c host.orb.internal -6 -R
+# iperf, ipv4, UDP, send
+docker exec -i iperf-client-test iperf3 -c host.orb.internal -4 -u -b 2000M
+# iperf, ipv4, UDP, receive
+docker exec -i iperf-client-test iperf3 -c host.orb.internal -4 -u -R -b 2000M
+# iperf, ipv6, UDP, send
+docker exec -i iperf-client-test iperf3 -c host.orb.internal -6 -u -b 2000M
+# iperf, ipv6, UDP, receive
+docker exec -i iperf-client-test iperf3 -c host.orb.internal -6 -u -R -b 2000M
+# fq_codel net sched stress by spamming UDP on many vCPUs
+docker exec -i iperf-client-test iperf3 -c host.orb.internal -u -b 10G -P 20
+docker stop iperf-client-test
+
+# pgbench on btrfs
+cd ~/code/vm/fsbench/pgbench
+make db
+make pgbench
+make clean
+
+# pgbench on virtiofs
+cd ~/code/projects/macvirt/exp/fsbench/pgbench
+make db
+make pgbench
+make clean
+
+# docker image build with rosetta
+cd ~/code/projects/macvirt/rootfs
+docker builder prune -a -f
+make amd64-debug
+
+# NFS server
+cd ~/OrbStack/arch/home/$USER/linux
+hyperfine 'rg hvc_put'
+
+# memory pressure: run until OOM, 2x
+cd ~/code/projects/macvirt
+orb exp/mem/memhog-linux
+orb exp/mem/memhog-linux
+
+# MGLRU aging + vfs cache pressure
+# disable tty, so we can background this
+ssh ovm apk add hyperfine
+orb -w "/home/$USER/linux" hyperfine 'rg -j1 hvc_put; fd -j1 -uuu' 2>&1 </dev/null | cat &
+sleep 1
+# same thing vinit writes
+ssh ovm hyperfine -m 50 "'sleep 0.1; echo - 1 0 18446744073709551615 0 > /sys/kernel/debug/lru_gen; echo + 1 0 18446744073709551615 0 0 > /sys/kernel/debug/lru_gen'"
+
+# run nginx for web tests
+docker run -d --rm --name ngbench -p 8123:80 nginx
+# make sure server is up
+sleep 2
+# TLS proxy
+hey -z 10s https://ngbench.orb.local
+# agent userspace TCP proxy
+hey -z 10s http://localhost:8123
+# internally in VM
+orb hey -z 10s http://docker.orb.internal:8123
+docker stop ngbench
+
+# stress pstub startup
+docker run -i --rm -p 29000-29500:29000-29500 alpine echo
+
+# stress orbstack tcp and udp userspace proxies
+# this uses epoll and futex
+docker run -d --rm --name iperf-test -p 5205:5201 -p 5205:5201/udp alpine sh -c 'apk add iperf3 && iperf3 -s'
+# make sure server is up
+sleep 5 # includes package downloads
+# iperf, ipv4, TCP, send
+iperf3 -c localhost -p 5205 -4
+# iperf, ipv4, TCP, receive
+iperf3 -c localhost -p 5205 -4 -R
+# iperf, ipv6, TCP, send
+iperf3 -c localhost -p 5205 -6
+# iperf, ipv6, TCP, receive
+iperf3 -c localhost -p 5205 -6 -R
+# iperf, ipv4, UDP, send
+iperf3 -c localhost -p 5205 -4 -u -b 2000M
+# iperf, ipv4, UDP, receive
+# TODO: fix this
+#iperf3 -c localhost -p 5205 -4 -u -R -b 2000M
+# iperf, ipv6, UDP, send
+iperf3 -c localhost -p 5205 -6 -u -b 2000M
+# iperf, ipv6, UDP, receive
+# TODO: fix this
+#iperf3 -c localhost -p 5205 -6 -u -R -b 2000M
+docker stop iperf-test
+
+# io_uring + fio + block stress
+orb -w /home/$USER bash ~/code/projects/macvirt/exp/benchmarks/disk/fio.sh
+
+# virtiofs clone with tiny reads/writes
+cd /Volumes/CaseSensitive
+rm -fr linux-ref-clone2
+orb git clone linux-ref-clone linux-ref-clone2
+rm -fr linux-ref-clone2
+
+# TODO: non-PI futex stress
+
+# singlestore DB (timerfd stress)
+docker run -d --rm --platform linux/amd64 --name pgo-singlestore -e ROOT_PASSWORD=test ghcr.io/singlestore-labs/singlestoredb-dev:latest
+sleep 30
+docker kill pgo-singlestore
+
+# rust build on virtiofs
+cd ~/code/projects/macvirt/vinit
+rm -fr target
+orb cargo build
+
+# clear k8s data (this disables k8s, so run it first)
+orb delete -f k8s || :
+# re-enable k8s
+orb start k8s
+sleep 15
+# deploy something
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+helm install bitnami/mysql --generate-name
+# TODO: this is broken on v1.7.0
+#helm install minio oci://registry-1.docker.io/bitnamicharts/minio --set provisioning.enabled=true || :
+sleep 5
+
+# explicit drop_caches eviction
+echo 3 | orb sudo tee /proc/sys/vm/drop_caches
+
+# node.js app
+cd ~/code/web/orbstack-web
+docker compose up -d
+# leave this running while we build posthog
+
+# PostHog docker build (native and Rosetta)
+cd ~/code/vm/posthog
+docker build . -f production.Dockerfile --platform linux/arm64
+docker build . -f production.Dockerfile --platform linux/amd64
+
+# test and stop node.js app
+cd ~/code/web/orbstack-web
+curl https://orbstack.local/
+docker compose down
+
+echo "END PGO BENCHMARK AT"
+date
+echo
+
+kill $iperf_server_pid
