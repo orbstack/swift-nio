5bf57827ff85d9fdbb2daa2feaa4af6eaf500522
Danny Lin <danny@kdrag0n.dev>
2024-08-28 23:54:41 -0700

    scon: fix "make run" workflow

--- scon/build.sh
+++ scon/build.sh
@@ -2,6 +2,9 @@
 
 set -euxo pipefail
 
+# allow updating Go toolchain from golang.org
+export GOTOOLCHAIN=auto
+
 # Apple M1 is ARMv8.4 + most v8.5 extensions (SB, SSBS, CCDP, FRINT3264, SPECRESTRICT, ALTERNATIVENZCV)
 # just use v8.4 for simplicity -- we mainly care about specializing for LSE atomics
 export GOARM64=v8.4
