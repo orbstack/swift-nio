adc6b762c0b5ffbca75059f52bbd3e13f0c6792d
Radbuglet <riley@orbstack.dev>
2024-09-01 15:32:48 -0600

    virtue: memory: write a few basic `GuestMemory` codegen tests

--- virtue/exp/libkrun-asm-tests/src/lib.rs
+++ virtue/exp/libkrun-asm-tests/src/lib.rs
@@ -1,9 +1,26 @@
+use utils::memory::{GuestRef, GuestSlice};
+
 #[no_mangle]
 pub fn increment(v: u32) -> u32 {
     v + 1
 }
 
 #[no_mangle]
-pub fn multiply_by_five(v: u32) -> u32 {
-    v * 5
+pub fn index_slice_inlined(v: &[u32]) -> u32 {
+    v[4]
+}
+
+#[no_mangle]
+pub fn index_guest_mem_inlined(v: GuestSlice<u32>) -> u32 {
+    v.get(4).read()
+}
+
+#[no_mangle]
+pub fn read_regular_ref(v: &u32) -> u32 {
+    *v
+}
+
+#[no_mangle]
+pub fn read_guest_ref(v: GuestRef<u32>) -> u32 {
+    v.read()
 }
