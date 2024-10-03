orbstack/swift-nio

commit 2cf232bbf94d40395f6a60304d3b3d2a2d08e679

scon: refactor/update configuration generation for NixOS machines
This commit:
- syncs the initial configuration.nix with upstream (which notably
  enables systemd-networkd)
- stops using a constant state version, instead using the version of the
  machine being created
- change orbstack.nix to note that we will make changes to it in the
  future (as opposed to may)
- enable documentation by default (closes SUP-395)
- move the user/machine-specific options to configuration.nix, so we can
  eventually start distributing updates for the orbstack.nix file
- create users declaratively (closes ENG-290)
- use embedded templates instead of long constant strings to generate the
  Nix files

Perhaps we should add a changelog entry asking that users re-create their
machines, but either way, new machines will now be more in-line with what
the upstream maintainers of the LXD images will be testing. 🎉

scon/agent/setup_nixos.go
@@ -1,127 +1,28 @@
 package agent
 
 import (
-	"fmt"
+	"bytes"
 	"os"
+	"regexp"
+	"strings"
 
+	"github.com/orbstack/macvirt/scon/agent/templates"
+	"github.com/orbstack/macvirt/scon/util"
 	"github.com/orbstack/macvirt/vmgr/conf/mounts"
 	"github.com/sirupsen/logrus"
 )
 
-const (
-	// changes:
-	// added import for modulesPath
-	// fixed lxc-container.nix include path
-	// added orbstack.nix
-	nixConfiguration = `# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ modulesPath, config, pkgs, lib, ... }:
-
-with lib;
-
-{
-  imports =
-    [ # Include the default lxd configuration.
-      "${modulesPath}/virtualisation/lxc-container.nix"
-      # Include the container-specific autogenerated configuration.
-      ./lxd.nix
-      ./orbstack.nix
-    ];
-
-  # networking.hostName = mkForce "nixos"; # Overwrite the hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.eth0.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  # };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  #   firefox
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.05"; # Did you read the comment?
-
-  # As this is intended as a stadalone image, undo some of the minimal profile stuff
-  documentation.enable = true;
-  documentation.nixos.enable = true;
-  environment.noXlibs = false;
-}
-`
-)
+var identifier = regexp.MustCompile(`^[a-zA-Z_][a-zA-Z0-9_'-]*$`)
 
 func configureSystemNixos(args InitialSetupArgs) error {
-	// write nix snippet
-	logrus.Debug("Writing nix snippet")
+	logrus.Debug("Writing orbstack.nix")
+
+	err := os.WriteFile("/etc/nixos/orbstack.nix", templates.OrbstackNix, 0644)
+	if err != nil {
+		return err
+	}
+
+	logrus.Debug("Writing configuration.nix")
 
 	// can't use builtins.readFile in pure flakes, w/o inputs.*
 	extraCertsData, err := os.ReadFile(mounts.ExtraCerts)
@@ -129,97 +30,41 @@ func configureSystemNixos(args InitialSetupArgs) error {
 		return err
 	}
 
-	// systemd-resolved and systemd-networkd removed from WatchdogSec list:
-	// nixos unstable (as of 02/22/2024) does not have these services,
-	// and adding WatchdogSec causes them to be generated with ONLY WatchdogSec and nothing else,
-	// causing invalid unit file
-	nixSnippet := fmt.Sprintf(`# Generated by OrbStack.
-# This MAY be overwritten in the future. Make a copy and update the include
-# in configuration.nix if you want to keep your changes.
-
-{ lib, config, pkgs, ... }:
-
-with lib;
-
-{
-  # sudoers
-  security.sudo.extraRules= [
-    { users = [ "%s" ];
-      commands = [
-        { command = "ALL";
-          options= [ "NOPASSWD" ];
-        }
-      ];
-    }
-  ];
-
-  # add OrbStack CLI tools to PATH
-  environment.shellInit = ''
-    . /opt/orbstack-guest/etc/profile-early
-
-    # add your customizations here
-
-    . /opt/orbstack-guest/etc/profile-late
-  '';
-
-  # timezone
-  time.timeZone = "%s";
-
-  # resolv.conf: NixOS doesn't use systemd-resolved
-
-  # faster DHCP - OrbStack uses SLAAC exclusively
-  networking.dhcpcd.extraConfig = ''
-    noarp
-    noipv6
-  '';
-
-  # disable sshd
-  services.openssh.enable = false;
-
-  # systemd
-  systemd.services."systemd-oomd".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-userdbd".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-udevd".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-timesyncd".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-timedated".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-portabled".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-nspawn@".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-machined".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-localed".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-logind".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-journald@".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-journald".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-journal-remote".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-journal-upload".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-importd".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-hostnamed".serviceConfig.WatchdogSec = 0;
-  systemd.services."systemd-homed".serviceConfig.WatchdogSec = 0;
-
-  # ssh config
-  programs.ssh.extraConfig = ''
-    Include /opt/orbstack-guest/etc/ssh_config
-  '';
-
-  # extra certificates
-  security.pki.certificates = [
-    ''
-%s
-    ''
-  ];
-
-  # indicate builder support for emulated architectures
-  nix.extraOptions = "extra-platforms = x86_64-linux i686-linux";
-}
-`, args.Username, args.Timezone, string(extraCertsData))
-
-	err = os.WriteFile("/etc/nixos/orbstack.nix", []byte(nixSnippet), 0644)
-	if err != nil {
-		return err
+	type configurationTemplateData struct {
+		Username     string
+		Password     string
+		NoPassword   bool
+		Timezone     string
+		Certificates string
+		StateVersion string
 	}
 
-	// add to configuration.nix and fix lxc-container module
-	logrus.Debug("Writing configuration.nix")
-	err = os.WriteFile("/etc/nixos/configuration.nix", []byte(nixConfiguration), 0644)
+	username := args.Username
+	if !identifier.MatchString(username) {
+		username = `"` + args.Username + `"`
+	}
+
+	password := ""
+	if args.Password != "" {
+		hashedPassword, err := util.RunWithInput(args.Password, "mkpasswd", "-s")
+		if err != nil {
+			return err
+		}
+
+		password = strings.TrimSpace(hashedPassword)
+	}
+
+	var configuration bytes.Buffer
+	err = templates.NixOSConfiguration.Execute(&configuration, configurationTemplateData{
+		Username:     username,
+		Password:     password,
+		NoPassword:   password == "",
+		Timezone:     args.Timezone,
+		Certificates: string(extraCertsData),
+		StateVersion: args.Version,
+	})
+
+	err = os.WriteFile("/etc/nixos/configuration.nix", configuration.Bytes(), 0644)
 	if err != nil {
 		return err
 	}

scon/agent/setup.go
@@ -406,8 +406,6 @@ func (a *AgentServer) createUserAndGroup(username string, uid int, gid int, shel
 	if _, err := strconv.Atoi(groupName); err == nil {
 		groupName = "g" + groupName
 	}
-	// NixOS can't have @
-	groupName = strings.ReplaceAll(groupName, "@", "_")
 	err := util.Run("groupadd", "--gid", gidStr, groupName)
 	if err != nil {
 		if errors.Is(err, exec.ErrNotFound) {
@@ -450,41 +448,66 @@ func (a *AgentServer) InitialSetup(args InitialSetupArgs, _ *None) error {
 		logrus.WithError(err).Warn("failed to wait for cloud-init")
 	}
 
-	// find a shell
-	shell, err := selectShell()
-	if err != nil {
-		return err
-	}
-
-	// delete default lxd image users to avoid conflict
-	err = deleteDefaultUsers()
-	if err != nil {
-		return err
-	}
-
-	// Alpine: install shadow early for standard "useradd" tool. Busybox checks uid <= 256000
-	if args.Distro == images.ImageAlpine {
-		// keep cache for other pkg installation
-		err = util.Run("apk", "add", "shadow")
+	if args.Distro == images.ImageNixos {
+		// have to do it all with nix instead
+		err = configureSystemNixos(args)
+		if err != nil {
+			logrus.WithError(err).Error("NixOS system configuration failed")
+			return err
+		}
+	} else {
+		// find a shell
+		shell, err := selectShell()
 		if err != nil {
 			return err
 		}
-	}
 
-	// create user group
-	gid := args.Uid
-	err = a.createUserAndGroup(args.Username, args.Uid, gid, shell)
-	// ignore if already exists
-	if err != nil && !strings.Contains(err.Error(), "already exists") {
-		return err
-	}
-
-	// set password
-	if args.Password != "" {
-		logrus.Debug("Setting password")
-		pwdEntries := args.Username + ":" + args.Password + "\nroot:" + args.Password + "\n"
-		err = util.RunWithInput(pwdEntries, "chpasswd")
+		// delete default lxd image users to avoid conflict
+		err = deleteDefaultUsers()
 		if err != nil {
+			return err
+		}
+
+		// Alpine: install shadow early for standard "useradd" tool. Busybox checks uid <= 256000
+		if args.Distro == images.ImageAlpine {
+			// keep cache for other pkg installation
+			err = util.Run("apk", "add", "shadow")
+			if err != nil {
+				return err
+			}
+		}
+
+		// create user group
+		gid := args.Uid
+		err = a.createUserAndGroup(args.Username, args.Uid, gid, shell)
+		// ignore if already exists
+		if err != nil && !strings.Contains(err.Error(), "already exists") {
+			return err
+		}
+
+		// set password
+		if args.Password != "" {
+			logrus.Debug("Setting password")
+			pwdEntries := args.Username + ":" + args.Password + "\nroot:" + args.Password + "\n"
+			_, err = util.RunWithInput(pwdEntries, "chpasswd")
+			if err != nil {
+				return err
+			}
+		}
+
+		// add user to admin groups
+		// Alpine has no usermod, so we have to do this manually
+		groups := selectAdminGroups()
+		logrus.WithField("groups", groups).Debug("Adding user to groups")
+		err = addUserToGroups(args.Username, groups)
+		if err != nil {
+			return err
+		}
+
+		// standard system configuration
+		err = configureSystemStandard(args)
+		if err != nil {
+			logrus.WithError(err).Error("standard system configuration failed")
 			return err
 		}
 	}
@@ -500,7 +523,7 @@ func (a *AgentServer) InitialSetup(args InitialSetupArgs, _ *None) error {
 	if err != nil {
 		return err
 	}
-	gid, err = strconv.Atoi(u.Gid)
+	gid, err := strconv.Atoi(u.Gid)
 	if err != nil {
 		return err
 	}
@@ -587,39 +610,11 @@ func (a *AgentServer) InitialSetup(args InitialSetupArgs, _ *None) error {
 		}
 	}
 
-	// add user to admin groups
-	// Alpine has no usermod, so we have to do this manually
-	groups := selectAdminGroups()
-	logrus.WithField("groups", groups).Debug("Adding user to groups")
-	err = addUserToGroups(args.Username, groups)
-	if err != nil {
-		return err
-	}
-
 	// create path translation disambiguation symlink at /mnt/linux
 	logrus.Debug("Creating /mnt/linux symlink")
 	err = os.Symlink("/", "/mnt/linux")
 	if err != nil {
 		return err
-	}
-
-	/*
-	 * after this point is system configs
-	 */
-	if args.Distro == images.ImageNixos {
-		// have to do it all with nix instead
-		err = configureSystemNixos(args)
-		if err != nil {
-			logrus.WithError(err).Error("NixOS system configuration failed")
-			return err
-		}
-	} else {
-		// standard system configuration
-		err = configureSystemStandard(args)
-		if err != nil {
-			logrus.WithError(err).Error("standard system configuration failed")
-			return err
-		}
 	}
 
 	return nil

scon/agent/templates/embed.go
@@ -1,0 +1,14 @@
+package templates
+
+import (
+	_ "embed"
+	"text/template"
+)
+
+//go:embed nixos-configuration.nix
+var nixOSConfigurationTemplate string
+
+//go:embed nixos-orbstack.nix
+var OrbstackNix []byte
+
+var NixOSConfiguration = template.Must(template.New("configuration.nix").Parse(nixOSConfigurationTemplate))

scon/agent/templates/nixos-configuration.nix
@@ -1,0 +1,59 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [
+      # Include the default lxd configuration.
+      "${modulesPath}/virtualisation/lxc-container.nix"
+      # Include the container-specific autogenerated configuration.
+      ./lxd.nix
+      # Include the OrbStack-specific configuration.
+      ./orbstack.nix
+    ];
+
+  users.users.{{.Username}} = {
+    isNormalUser = true;{{if .Password}}
+    hashedPassword = "{{.Password}}";{{end}}
+    extraGroups = [ "wheel" ];
+  };{{if .NoPassword}}
+
+  security.sudo.wheelNeedsPassword = false;{{end}}{{if .Password}}
+
+  users.users.root.hashedPassword = "{{.Password}}";{{end}}
+
+  # This being `true` leads to a few nasty bugs, change at your own risk!
+  users.mutableUsers = false;
+
+  time.timeZone = "{{.Timezone}}";
+
+  networking = {
+    dhcpcd.enable = false;
+    useDHCP = false;
+    useHostResolvConf = false;
+  };
+
+  systemd.network = {
+    enable = true;
+    networks."50-eth0" = {
+      matchConfig.Name = "eth0";
+      networkConfig = {
+        DHCP = "ipv4";
+        IPv6AcceptRA = true;
+      };
+      linkConfig.RequiredForOnline = "routable";
+    };
+  };{{if .Certificates}}
+
+  # Extra certificates from OrbStack.
+  security.pki.certificates = [
+    ''
+      {{.Certificates}}
+    ''
+  ];{{end}}
+
+  system.stateVersion = "{{.StateVersion}}"; # Did you read the comment?
+}

scon/agent/templates/nixos-orbstack.nix
@@ -1,0 +1,65 @@
+# Generated by OrbStack.
+# This WILL be overwritten in the future. Make a copy and update the include
+# in configuration.nix if you want to keep your changes.
+
+{ lib, config, ... }:
+
+{
+  # Add OrbStack CLI tools to PATH
+  environment.shellInit = ''
+    . /opt/orbstack-guest/etc/profile-early
+
+    # add your customizations here
+
+    . /opt/orbstack-guest/etc/profile-late
+  '';
+
+  # Enable documentation
+  documentation.man.enable = true;
+  documentation.doc.enable = true;
+  documentation.info.enable = true;
+
+  # Disable systemd-resolved
+  services.resolved.enable = false;
+  environment.etc."resolv.conf".source = "/opt/orbstack-guest/etc/resolv.conf";
+
+  # Faster DHCP - OrbStack uses SLAAC exclusively
+  networking.dhcpcd.extraConfig = ''
+    noarp
+    noipv6
+  '';
+
+  # Disable sshd
+  services.openssh.enable = false;
+
+  # systemd
+  systemd.services."systemd-oomd".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-userdbd".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-udevd".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-timesyncd".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-timedated".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-portabled".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-nspawn@".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-machined".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-localed".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-logind".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-journald@".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-journald".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-journal-remote".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-journal-upload".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-importd".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-hostnamed".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-homed".serviceConfig.WatchdogSec = 0;
+  systemd.services."systemd-networkd".serviceConfig.WatchdogSec = lib.mkIf config.systemd.network.enable 0;
+
+  # ssh config
+  programs.ssh.extraConfig = ''
+    Include /opt/orbstack-guest/etc/ssh_config
+  '';
+
+  # indicate builder support for emulated architectures
+  nix.settings.extra-platforms = [
+    "x86_64-linux"
+    "i686-linux"
+  ];
+}

scon/util/exec.go
@@ -33,16 +33,16 @@ func RunWithOutput(combinedArgs ...string) (string, error) {
 	return string(output), nil
 }
 
-func RunWithInput(input string, combinedArgs ...string) error {
+func RunWithInput(input string, combinedArgs ...string) (string, error) {
 	cmd := exec.Command(combinedArgs[0], combinedArgs[1:]...)
 	cmd.Stdin = strings.NewReader(input)
 	// inherits env
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return fmt.Errorf("run command '%v': %w; output: %s", combinedArgs, err, string(output))
+		return "", fmt.Errorf("run command '%v': %w; output: %s", combinedArgs, err, string(output))
 	}
 
-	return nil
+	return string(output), nil
 }
 
 func RunInheritOut(combinedArgs ...string) error {

commit 436256af672578099cc73f89b6bd61a8b7c80a69

scon: don't use an invalid state version when creating a NixOS unstable machine
Regressed in 2cf232bbf94d40395f6a60304d3b3d2a2d08e679.

scon/agent/setup_nixos.go
@@ -71,6 +71,32 @@ func configureSystemNixos(args InitialSetupArgs) error {
 		password = strings.TrimSpace(hashedPassword)
 	}
 
+	lsbRelease, err := os.ReadFile("/etc/lsb-release")
+	if err != nil {
+		return err
+	}
+
+	stateVersion := ""
+	for _, line := range strings.Split(string(lsbRelease), "\n") {
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		key, value, valid := strings.Cut(line, "=")
+		if !valid {
+			continue
+		}
+
+		if strings.TrimSpace(key) == "DISTRIB_RELEASE" {
+			stateVersion = strings.TrimPrefix(strings.TrimSuffix(strings.TrimSpace(value), `"`), `"`)
+			break
+		}
+	}
+
+	if stateVersion == "" {
+		return errors.New("couldn't find DISTRIB_RELEASE in /etc/lsb-release")
+	}
+
 	var configuration bytes.Buffer
 	err = templates.NixOSConfiguration.Execute(&configuration, configurationTemplateData{
 		ConfigFile:   configFile,
@@ -80,7 +106,7 @@ func configureSystemNixos(args InitialSetupArgs) error {
 		NoPassword:   password == "",
 		Timezone:     args.Timezone,
 		Certificates: string(extraCertsData),
-		StateVersion: args.Version,
+		StateVersion: stateVersion,
 		UID:          args.Uid,
 	})
 	if err != nil {