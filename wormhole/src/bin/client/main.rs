856308f6cf091cfcc9be53dbb9d3b033a23c735b
Kevin Liu <kevin@orbstack.dev>
2024-10-04 17:26:58 -0700

    rewrite client in async tokio

--- wormhole/src/bin/client/main.rs
+++ wormhole/src/bin/client/main.rs
@@ -1,91 +1,92 @@
-use anyhow::anyhow;
-use core::str;
-use libc::{sendmsg, tls12_crypto_info_chacha20_poly1305};
-use nix::sys::socket::{recv, send, MsgFlags};
-use nix::sys::termios::{cfmakeraw, tcgetattr, tcsetattr, InputFlags, LocalFlags, SetArg};
-use serde::{Deserialize, Serialize};
-use std::io::{prelude::*, stderr, stdin, stdout};
-use std::net::TcpStream;
-use std::os::unix::net::UnixStream;
-use std::thread;
-use std::{io::prelude::*, os::fd::AsRawFd};
-use wormhole::TermiosParams;
+use anyhow::{anyhow, Result};
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use tokio::net::UnixStream;
+use tokio::sync::{broadcast, mpsc};
+use tokio::task;
 
-use nix::libc::{cc_t, tcflag_t, NCCS};
-use nix::unistd::{dup2, sleep};
+#[tokio::main]
+async fn main() -> Result<()> {
+    let socket = UnixStream::connect("/rpc.sock")
+        .await
+        .map_err(|e| anyhow!("Could not connect to RPC socket: {}", e))?;
 
-struct Cleanup;
+    let (mut socket_reader, mut socket_writer) = socket.into_split();
+    let (shutdown_tx, _) = broadcast::channel(1);
 
-impl Drop for Cleanup {
-    fn drop(&mut self) {
-        // todo
-    }
-}
+    let to_socket = task::spawn({
+        let mut socket_writer = socket_writer;
+        let shutdown_tx = shutdown_tx.clone();
+        let mut shutdown_rx = shutdown_tx.subscribe();
+        async move {
+            let mut stdin = io::stdin();
+            let mut buf = [0u8; 1024];
 
-fn main() -> anyhow::Result<()> {
-    let STDIN_FD = stdin();
-    // let mut termios = tcgetattr(&STDIN_FD)?;
-    // cfmakeraw(&mut termios);
-    // termios.local_flags.remove(LocalFlags::ICANON);
-    // termios.local_flags.remove(LocalFlags::ECHO);
-    // termios.local_flags.remove(LocalFlags::ECHONL);
-    // tcsetattr(&STDIN_FD, SetArg::TCSAFLUSH, &termios)?;
-
-    // println!("termios {:?}", termios.local_flags);
-    // let host_termios = std::env::args().nth(1).unwrap();
-    // let mut host_termios_params = serde_json::from_str::<TermiosParams>(&host_termios)?;
-
-    // println!("host termios settings {:?}", host_termios_params);
-
-    let rpc_server_socket = "/rpc.sock";
-
-    let mut socket = match UnixStream::connect(rpc_server_socket) {
-        Ok(sock) => sock,
-        Err(err) => return Err(anyhow!("could not connect to rpc sock")),
-    };
-
-    let socket_fd = socket.as_raw_fd();
-
-    // let termios_payload = bincode::serialize(&host_termios_params)?;
-    // let len_bytes = u32::try_from(termios_payload.len())?.to_be_bytes();
-    // send(socket_fd, &len_bytes, MsgFlags::empty())?;
-    // send(socket_fd, &termios_payload, MsgFlags::empty())?;
-
-    let to_socket = thread::spawn(move || -> anyhow::Result<()> {
-        let mut stdin_lock = stdin().lock();
-        let mut buf = [0u8; 1024];
-
-        loop {
-            let n = stdin_lock.read(&mut buf)?;
-            if n == 0 {
-                // eof from client
-                break;
+            loop {
+                tokio::select! {
+                    result = stdin.read(&mut buf) => {
+                        match result {
+                            Ok(0) => {
+                                socket_writer.shutdown().await?;
+                                let _= shutdown_tx.send(());
+                                break;
+                            }
+                            Ok(n) => {
+                                socket_writer.write_all(&buf[..n]).await?;
+                            }
+                            Err(e) => {
+                                return Err(anyhow!("Error reading from stdin: {}", e));
+                            }
+                        }
+                    }
+                    _ = shutdown_rx.recv() => {
+                        break;
+                    }
+                }
             }
 
-            send(socket_fd, &buf[..n], MsgFlags::empty())?;
+            Ok::<(), anyhow::Error>(())
         }
-        Ok(())
     });
 
-    let from_socket = thread::spawn(move || -> anyhow::Result<()> {
-        let mut stdout_lock = stdout().lock();
-        let mut buf = [0u8; 1024];
+    let from_socket = task::spawn({
+        let mut socket_reader = socket_reader;
+        let shutdown_tx = shutdown_tx.clone();
+        let mut shutdown_rx = shutdown_tx.subscribe();
+        async move {
+            let mut stdout = io::stdout();
+            let mut buf = [0u8; 1024];
 
-        loop {
-            let n = recv(socket_fd, &mut buf, MsgFlags::empty())?;
-            if n == 0 {
-                // eof from server
-                break;
+            loop {
+                tokio::select! {
+                    result = socket_reader.read(&mut buf) => {
+                        match result {
+                            Ok(0) => {
+                                let _ = shutdown_tx.send(());
+                                break;
+                            }
+                            Ok(n) => {
+                                stdout.write_all(&buf[..n]).await?;
+                                stdout.flush().await?;
+                            }
+                            Err(e) => {
+                                return Err(anyhow!("Error reading from socket: {}", e));
+                            }
+                        }
+                    }
+                    _ = shutdown_rx.recv() => {
+                        break;
+                    }
+                }
             }
-            stdout_lock.write_all(&buf[..n])?;
-            stdout_lock.flush()?;
-        }
 
-        Ok(())
+            Ok::<(), anyhow::Error>(())
+        }
     });
 
-    let _ = to_socket.join();
-    let _ = from_socket.join();
+    let (to_result, from_result) = tokio::join!(to_socket, from_socket);
+
+    to_result??;
+    from_result??;
 
     Ok(())
 }
