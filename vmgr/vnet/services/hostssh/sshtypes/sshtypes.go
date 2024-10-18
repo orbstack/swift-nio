3fcd8ba39cdd01e2c4dc134d5f6b290d0ab61c9f
Kevin Liu <kevin@orbstack.dev>
2024-10-17 22:22:58 -0700

    fix: string -> bool in wormhole types

--- vmgr/vnet/services/hostssh/sshtypes/sshtypes.go
+++ vmgr/vnet/services/hostssh/sshtypes/sshtypes.go
@@ -30,5 +30,5 @@ type WormholeConfig struct {
 	ContainerEnv     []string `json:"g"`
 
 	EntryShellCmd string `json:"h,omitempty"`
-	IsLocal       string `json:"i"`
+	IsLocal       bool   `json:"i"`
 }
