1bb24a794d81d0a80b1e439d6009b84904bb81f9
Danny Lin <danny@kdrag0n.dev>
2024-10-31 07:54:09 -0700

    wormhole: stub: specialize for stopped container use case

--- wormhole/wormhole-stub.c
+++ wormhole/wormhole-stub.c
@@ -1,24 +1,43 @@
 /*
- * Generic stub process that sleeps forever. Compiled as wormhole-stub for clarity.
+ * Stub process that sleeps forever.
  *
- * This blocks forever, or until SIGTERM/SIGINT/SIGQUIT is received. Works as both pid 1 and as a normal process.
+ * This blocks forever, or until SIGTERM/SIGINT/SIGQUIT is received.
  */
 
-#include <signal.h>
 #include <poll.h>
+#include <signal.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/prctl.h>
+#include <unistd.h>
 
 void signal_handler(int sig) {
     // we could rely on EINTR + non-SA_RESTART handlers, but this is simpler
     exit(0);
 }
 
-int main() {
-    // if running as pid 1, SIG_DFL = SIG_IGN, so we need a signal handler in order to exit on signal
+int main(int argc, char **argv) {
+    // in pid 1, SIG_DFL = SIG_IGN, so we need a signal handler in order to exit on signal
     signal(SIGTERM, signal_handler);
     signal(SIGINT, signal_handler);
     signal(SIGQUIT, signal_handler);
+
+    if (getpid() != 1) {
+        // helpful message if users see this in `ps` and try to run it
+        printf("This is an internal helper process for OrbStack Debug Shell.\n"
+               "Run `entrypoint` to start the container.\n");
+        exit(0);
+    }
+
+    // set vanity name
+    int ret = prctl(PR_SET_NAME, "(entrypoint)");
+    if (ret != 0) {
+        perror("prctl");
+        exit(1);
+    }
+    strcpy(argv[0], "(run `entrypoint` to start container)");
 
     // block forever
     while (1) {
