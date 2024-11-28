orbstack/swift-nio

commit 78f720a99483c05bc0d7dad1fe929f363ac68d3a

scon: fix domain name not getting re-added after restart: orbstack/orbstack#1553

scon/rpc_guest.go
@@ -144,6 +144,14 @@ func (s *SconGuestServer) onDockerContainerRemovedFromCache(cid string) error {
 	}
 
 	logrus.WithField("cid", cid).Debug("removing container due to restart")
+
+	err := s.dockerMachine.UseAgent(func(c *agent.Client) error {
+		return c.DockerRemoveContainerFromCache(cid)
+	})
+	if err != nil {
+		return err
+	}
+
 	return s.onDockerContainersChangedLocked(sgtypes.ContainersDiff{
 		Diff: sgtypes.Diff[dockertypes.ContainerSummaryMin]{
 			Added:   nil,

scon/agent/client.go
@@ -455,3 +455,13 @@ func (c *Client) UpdateHostname(oldName string, newName string) error {
 
 	return nil
 }
+
+func (c *Client) DockerRemoveContainerFromCache(cid string) error {
+	var none None
+	err := c.rpc.Call("a.DockerRemoveContainerFromCache", cid, &none)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

scon/agent/docker_containers.go
@@ -239,3 +239,16 @@ func (d *DockerAgent) onContainerStop(ctr dockertypes.ContainerSummaryMin) error
 
 	return nil
 }
+
+func (a *AgentServer) DockerRemoveContainerFromCache(cid string, reply *None) error {
+	newLastContainers := make([]dockertypes.ContainerSummaryMin, 0, max(len(a.docker.lastContainers)-1, 0))
+	for _, ctr := range a.docker.lastContainers {
+		if ctr.ID != cid {
+			newLastContainers = append(newLastContainers, ctr)
+		}
+	}
+
+	a.docker.lastContainers = newLastContainers
+
+	return nil
+}

commit 408f89db2a31390e5bde5ed3f4980ba6f503a5c1

scon: fix deadlock introduced by 78f720a99483c05bc0d7dad1fe929f363ac68d3a
this fixes a lock ordering deadlock introduced when i tried to use agent
with the scon guest server lock held.

```
goroutine 5992 gp=0x40002c8fc0 m=nil [sync.RWMutex.RLock]:
runtime.gopark(0x4000337bf8?, 0x8122a8?, 0x0?, 0x0?, 0x4000337bd0?)
        /usr/lib/go/src/runtime/proc.go:424 +0xc8 fp=0x4000337bd0 sp=0x4000337bb0 pc=0x472778
runtime.goparkunlock(...)
        /usr/lib/go/src/runtime/proc.go:430
runtime.semacquire1(0x4000404474, 0x0, 0x3, 0x0, 0x16)
        /usr/lib/go/src/runtime/sema.go:178 +0x1c4 fp=0x4000337c20 sp=0x4000337bd0 pc=0x44fbd4
sync.runtime_SemacquireRWMutexR(0x812300?, 0xa0?, 0x4000554000?)
        /usr/lib/go/src/runtime/sema.go:100 +0x28 fp=0x4000337c60 sp=0x4000337c20 pc=0x473e48
sync.(*RWMutex).RLock(...)
        /usr/lib/go/src/sync/rwmutex.go:72
main.(*Container).acquireAgent(0x4000404380, 0x1, 0x0?)
        /mnt/mac/Users/dragon/code/projects/macvirt/scon/container.go:255 +0x64 fp=0x4000337cd0 sp=0x4000337c60 pc=0xac0414
main.(*Container).useAgentInternal(0x4000174230?, 0x4000337e48, 0x60?, 0xee?)
        /mnt/mac/Users/dragon/code/projects/macvirt/scon/container.go:286 +0x34 fp=0x4000337d30 sp=0x4000337cd0 pc=0xac06d4
main.(*Container).UseAgent(...)
        /mnt/mac/Users/dragon/code/projects/macvirt/scon/container.go:304
main.(*SconGuestServer).onDockerContainerRemovedFromCache(0x4000425040, {0x4000adc0c0, 0x40})
        /mnt/mac/Users/dragon/code/projects/macvirt/scon/rpc_guest.go:148 +0x1a0 fp=0x4000337ee0 sp=0x4000337d30 pc=0xaf4330
main.(*RuncWrapServer).handleConn(0x40003b04c8, {0xe51558, 0x4000444010})
        /mnt/mac/Users/dragon/code/projects/macvirt/scon/docker_runc_wrap.go:82 +0x118 fp=0x4000337f80 sp=0x4000337ee0 pc=0xacd2f8
main.(*RuncWrapServer).Serve.func1()
        /mnt/mac/Users/dragon/code/projects/macvirt/scon/docker_runc_wrap.go:39 +0x28 fp=0x4000337fd0 sp=0x4000337f80 pc=0xacd158
runtime.goexit({})

scon/rpc_guest.go
@@ -132,6 +132,13 @@ func (s *SconGuestServer) recvAndMountRootfsFdxLocked(ctr *dockertypes.Container
 }
 
 func (s *SconGuestServer) onDockerContainerRemovedFromCache(cid string) error {
+	err := s.dockerMachine.UseAgent(func(a *agent.Client) error {
+		return a.DockerRemoveContainerFromCache(cid)
+	})
+	if err != nil {
+		return err
+	}
+
 	// needs mutex! called from both scon guest rpc and from runc wrap server
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -144,13 +151,6 @@ func (s *SconGuestServer) onDockerContainerRemovedFromCache(cid string) error {
 	}
 
 	logrus.WithField("cid", cid).Debug("removing container due to restart")
-
-	err := s.dockerMachine.UseAgent(func(c *agent.Client) error {
-		return c.DockerRemoveContainerFromCache(cid)
-	})
-	if err != nil {
-		return err
-	}
 
 	return s.onDockerContainersChangedLocked(sgtypes.ContainersDiff{
 		Diff: sgtypes.Diff[dockertypes.ContainerSummaryMin]{