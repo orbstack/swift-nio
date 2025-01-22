e3cdcb582a5e056c199fc6cc4a12cbf83501e0a9
Danny Lin <danny@kdrag0n.dev>
2025-01-22 03:06:45 -0800

    starry: cp: avoid fchown() if euid/egid matches desired owner

--- rootfs/utils/starry/src/bin/starry-cp.rs
+++ rootfs/utils/starry/src/bin/starry-cp.rs
@@ -7,6 +7,7 @@ use std::{
 };
 
 use anyhow::{anyhow, Context};
+use libc::{getegid, geteuid};
 use nix::{
     errno::Errno,
     fcntl::{openat, OFlag},
@@ -24,6 +25,230 @@ use starry::{
         file::fchownat, getdents::{for_each_getdents, DirEntry, FileType}, inode_flags::InodeFlags, xattr::{fsetxattr, lsetxattr}
     },
 };
+
+struct CopyContext {
+    euid: u32,
+    egid: u32,
+    buffer_stack: BufferStack,
+}
+
+impl CopyContext {
+    fn new() -> anyhow::Result<Self> {
+        Ok(Self {
+            euid: unsafe { geteuid() },
+            egid: unsafe { getegid() },
+            buffer_stack: BufferStack::new()?,
+        })
+    }
+
+    fn do_one_entry(
+        &self,
+        src_dirfd: &OwnedFd,
+        dest_dirfd: &OwnedFd,
+        entry: &DirEntry,
+    ) -> anyhow::Result<()> {
+        let src = InterrogatedFile::from_entry(src_dirfd, entry)?;
+
+        // create dest
+        let permissions = Mode::from_bits_retain(src.st.st_mode & !libc::S_IFMT);
+        let dest_fd = match src.file_type {
+            // simple device types
+            FileType::Fifo => {
+                mkfifoat(Some(dest_dirfd.as_raw_fd()), entry.name, permissions)
+                    .context("mkfifoat")?;
+                None
+            }
+            FileType::Block => {
+                mknodat(
+                    Some(dest_dirfd.as_raw_fd()),
+                    entry.name,
+                    SFlag::S_IFBLK,
+                    permissions,
+                    src.st.st_rdev,
+                ).context("mknodat")?;
+                None
+            }
+            FileType::Char => {
+                mknodat(
+                    Some(dest_dirfd.as_raw_fd()),
+                    entry.name,
+                    SFlag::S_IFCHR,
+                    permissions,
+                    src.st.st_rdev,
+                ).context("mknodat")?;
+                None
+            }
+
+            // more complicated special types: symlink, socket
+            FileType::Symlink => {
+                src.with_readlink(|link_path| {
+                    symlinkat(link_path, Some(dest_dirfd.as_raw_fd()), entry.name)
+                }).context("readlink")?.context("symlinkat")?;
+                None
+            }
+            FileType::Socket => {
+                // sockets are uncommon, so this can be slow (String allocation + extra listen syscall)
+                let fd_path = format!(
+                    "/proc/self/fd/{}/{}",
+                    dest_dirfd.as_raw_fd(),
+                    entry.name.to_string_lossy()
+                );
+                _ = UnixListener::bind(&fd_path)?;
+
+                // can't specify mode as part of bind/listen
+                fchmodat(
+                    Some(dest_dirfd.as_raw_fd()),
+                    entry.name,
+                    permissions,
+                    FchmodatFlags::NoFollowSymlink,
+                ).context("fchmodat")?;
+
+                None
+            }
+
+            // regular files and directories
+            FileType::Regular => {
+                let fd = openat(
+                    Some(dest_dirfd.as_raw_fd()),
+                    entry.name,
+                    OFlag::O_CREAT | OFlag::O_WRONLY | OFlag::O_CLOEXEC,
+                    permissions,
+                ).context("openat")?;
+                let fd = unsafe { OwnedFd::from_raw_fd(fd) };
+                Some(fd)
+            }
+            FileType::Directory => {
+                mkdirat(Some(dest_dirfd.as_raw_fd()), entry.name, permissions)
+                    .context("mkdirat")?;
+
+                // TODO: empty dirs don't need to be opened if no inode flags
+                let fd = openat(
+                    Some(dest_dirfd.as_raw_fd()),
+                    entry.name,
+                    OFlag::O_RDONLY | OFlag::O_DIRECTORY | OFlag::O_CLOEXEC,
+                    permissions,
+                ).context("openat")?;
+                let fd = unsafe { OwnedFd::from_raw_fd(fd) };
+                Some(fd)
+            }
+
+            FileType::Unknown => unreachable!(),
+        };
+
+        // metadata: uid/gid, atime/mtime, xattrs, inode flags
+        if let Some(ref fd) = dest_fd {
+            // we have an open fd; use it for perf
+
+            // only call fchown if different from current fsuid/fsgid
+            // TODO: is changing fsuid/fsgid before creation faster?
+            if src.st.st_uid != self.euid || src.st.st_gid != self.egid {
+                fchown(fd, Some(src.st.st_uid), Some(src.st.st_gid))
+                    .context("fchown")?;
+            }
+
+            // no point in doing this lazily: with nsec, it'll never match src
+            futimens(
+                fd.as_raw_fd(),
+                &TimeSpec::new(src.st.st_atime, src.st.st_atime_nsec),
+                &TimeSpec::new(src.st.st_mtime, src.st.st_mtime_nsec),
+            ).context("futimens")?;
+
+            // suid/sgid gets cleared after chown
+            if permissions.contains(Mode::S_ISUID) || permissions.contains(Mode::S_ISGID) {
+                fchmod(fd.as_raw_fd(), permissions)
+                    .context("fchmod")?;
+            }
+
+            src.for_each_xattr(|name, value| {
+                fsetxattr(fd, name, value, 0)
+            }).context("listxattr/setxattr")?;
+
+            if let Some(flags) = src.inode_flags()? {
+                // filter to flags that are included in tar archives
+                // otherwise we'll be setting flags on every file if btrfs has nocow/compress enabled
+                let fl = flags.intersection(InodeFlags::ARCHIVE_FLAGS);
+                if !fl.is_empty() {
+                    fl.apply(fd).context("ioctl(FS_IOC_SETFLAGS)")?;
+                }
+            }
+        } else {
+            // don't have fd; fall back to path
+            // TODO: faster to open(O_PATH)?
+
+            // only call fchown if different from current fsuid/fsgid
+            if src.st.st_uid != self.euid || src.st.st_gid != self.egid {
+                fchownat(
+                    dest_dirfd,
+                    entry.name,
+                    src.st.st_uid,
+                    src.st.st_gid,
+                    libc::AT_SYMLINK_NOFOLLOW,
+                ).context("fchownat")?;
+            }
+
+            // no point in doing this lazily: with nsec, it'll never match src
+            utimensat(
+                Some(dest_dirfd.as_raw_fd()),
+                entry.name,
+                &TimeSpec::new(src.st.st_atime, src.st.st_atime_nsec),
+                &TimeSpec::new(src.st.st_mtime, src.st.st_mtime_nsec),
+                UtimensatFlags::NoFollowSymlink,
+            ).context("utimensat")?;
+
+            // suid/sgid gets cleared after chown
+            if permissions.contains(Mode::S_ISUID) || permissions.contains(Mode::S_ISGID) {
+                fchmodat(
+                    Some(dest_dirfd.as_raw_fd()),
+                    entry.name,
+                    permissions,
+                    FchmodatFlags::NoFollowSymlink,
+                ).context("fchmodat")?;
+            }
+
+            src.for_each_xattr(|name, value| {
+                // slowpath: most files only have 0 or 1 xattrs, so need to dedupe fd path creation
+                with_fd_path(dest_dirfd, entry.name, |path_cstr| {
+                    lsetxattr(path_cstr, name, value, 0)
+                })
+            }).context("listxattr/setxattr")?;
+        }
+
+        // file contents: only support reflinking
+        if src.file_type == FileType::Regular {
+            copy_regular_file_contents(&src.st, src.fd.as_ref().unwrap(), dest_fd.as_ref().unwrap())?;
+        }
+
+        // recurse into non-empty directories
+        if src.has_children() {
+            self.walk_dir(src.fd.as_ref().unwrap(), src.nents_hint(), dest_fd.as_ref().unwrap())?;
+        }
+
+        Ok(())
+    }
+
+    fn walk_dir(
+        &self,
+        src_dirfd: &OwnedFd,
+        src_nents_hint: Option<usize>,
+        dest_dirfd: &OwnedFd,
+    ) -> anyhow::Result<()> {
+        for_each_getdents(src_dirfd, src_nents_hint, &self.buffer_stack, |entry| {
+            self.do_one_entry(src_dirfd, dest_dirfd, &entry).map_err(|e| {
+                if e.is::<nix::Error>() {
+                    // nix::Error = root cause
+                    // start chain: "PATH: ERROR"
+                    anyhow!("{}: {}", entry.name.to_string_lossy(), e)
+                } else {
+                    // as we unwind the directory stack, prepend dirs to error
+                    // chain: "DIR/CHILD: ERROR"
+                    anyhow!("{}/{}", entry.name.to_string_lossy(), e)
+                }
+            })
+        })?;
+
+        Ok(())
+    }
+}
 
 fn copy_regular_file_contents(src_st: &libc::stat, src_fd: &OwnedFd, dest_fd: &OwnedFd) -> anyhow::Result<()> {
     // 1. attempt ioctl(FICLONE) for copy-on-write reflink
@@ -66,208 +291,6 @@ fn copy_regular_file_contents(src_st: &libc::stat, src_fd: &OwnedFd, dest_fd: &O
     Ok(())
 }
 
-fn do_one_entry(
-    src_dirfd: &OwnedFd,
-    dest_dirfd: &OwnedFd,
-    entry: &DirEntry,
-    buffer_stack: &BufferStack,
-) -> anyhow::Result<()> {
-    let src = InterrogatedFile::from_entry(src_dirfd, entry)?;
-
-    // create dest
-    let permissions = Mode::from_bits_retain(src.st.st_mode & !libc::S_IFMT);
-    let dest_fd = match src.file_type {
-        // simple device types
-        FileType::Fifo => {
-            mkfifoat(Some(dest_dirfd.as_raw_fd()), entry.name, permissions)
-                .context("mkfifoat")?;
-            None
-        }
-        FileType::Block => {
-            mknodat(
-                Some(dest_dirfd.as_raw_fd()),
-                entry.name,
-                SFlag::S_IFBLK,
-                permissions,
-                src.st.st_rdev,
-            ).context("mknodat")?;
-            None
-        }
-        FileType::Char => {
-            mknodat(
-                Some(dest_dirfd.as_raw_fd()),
-                entry.name,
-                SFlag::S_IFCHR,
-                permissions,
-                src.st.st_rdev,
-            ).context("mknodat")?;
-            None
-        }
-
-        // more complicated special types: symlink, socket
-        FileType::Symlink => {
-            src.with_readlink(|link_path| {
-                symlinkat(link_path, Some(dest_dirfd.as_raw_fd()), entry.name)
-            }).context("readlink")?.context("symlinkat")?;
-            None
-        }
-        FileType::Socket => {
-            // sockets are uncommon, so this can be slow (String allocation + extra listen syscall)
-            let fd_path = format!(
-                "/proc/self/fd/{}/{}",
-                dest_dirfd.as_raw_fd(),
-                entry.name.to_string_lossy()
-            );
-            _ = UnixListener::bind(&fd_path)?;
-
-            // can't specify mode as part of bind/listen
-            fchmodat(
-                Some(dest_dirfd.as_raw_fd()),
-                entry.name,
-                permissions,
-                FchmodatFlags::NoFollowSymlink,
-            ).context("fchmodat")?;
-
-            None
-        }
-
-        // regular files and directories
-        FileType::Regular => {
-            let fd = openat(
-                Some(dest_dirfd.as_raw_fd()),
-                entry.name,
-                OFlag::O_CREAT | OFlag::O_WRONLY | OFlag::O_CLOEXEC,
-                permissions,
-            ).context("openat")?;
-            let fd = unsafe { OwnedFd::from_raw_fd(fd) };
-            Some(fd)
-        }
-        FileType::Directory => {
-            mkdirat(Some(dest_dirfd.as_raw_fd()), entry.name, permissions)
-                .context("mkdirat")?;
-
-            // TODO: empty dirs don't need to be opened if no inode flags
-            let fd = openat(
-                Some(dest_dirfd.as_raw_fd()),
-                entry.name,
-                OFlag::O_RDONLY | OFlag::O_DIRECTORY | OFlag::O_CLOEXEC,
-                permissions,
-            ).context("openat")?;
-            let fd = unsafe { OwnedFd::from_raw_fd(fd) };
-            Some(fd)
-        }
-
-        FileType::Unknown => unreachable!(),
-    };
-
-    // metadata: uid/gid, atime/mtime, xattrs, inode flags
-    if let Some(ref fd) = dest_fd {
-        // we have an open fd; use it for perf
-
-        // TODO: skip if already matching current fsuid/fsgid. or is changing fsuid/fsgid faster?
-        fchown(fd, Some(src.st.st_uid), Some(src.st.st_gid))
-            .context("fchown")?;
-
-        // no point in doing this lazily: with nsec, it'll never match src
-        futimens(
-            fd.as_raw_fd(),
-            &TimeSpec::new(src.st.st_atime, src.st.st_atime_nsec),
-            &TimeSpec::new(src.st.st_mtime, src.st.st_mtime_nsec),
-        ).context("futimens")?;
-
-        // suid/sgid gets cleared after chown
-        if permissions.contains(Mode::S_ISUID) || permissions.contains(Mode::S_ISGID) {
-            fchmod(fd.as_raw_fd(), permissions)
-                .context("fchmod")?;
-        }
-
-        src.for_each_xattr(|name, value| {
-            fsetxattr(fd, name, value, 0)
-        }).context("listxattr/setxattr")?;
-
-        if let Some(flags) = src.inode_flags()? {
-            // filter to flags that are included in tar archives
-            // otherwise we'll be setting flags on every file if btrfs has nocow/compress enabled
-            let fl = flags.intersection(InodeFlags::ARCHIVE_FLAGS);
-            if !fl.is_empty() {
-                fl.apply(fd).context("ioctl(FS_IOC_SETFLAGS)")?;
-            }
-        }
-    } else {
-        // don't have fd; fall back to path
-        // TODO: faster to open(O_PATH)?
-
-        fchownat(
-            dest_dirfd,
-            entry.name,
-            src.st.st_uid,
-            src.st.st_gid,
-            libc::AT_SYMLINK_NOFOLLOW,
-        ).context("fchownat")?;
-
-        // no point in doing this lazily: with nsec, it'll never match src
-        utimensat(
-            Some(dest_dirfd.as_raw_fd()),
-            entry.name,
-            &TimeSpec::new(src.st.st_atime, src.st.st_atime_nsec),
-            &TimeSpec::new(src.st.st_mtime, src.st.st_mtime_nsec),
-            UtimensatFlags::NoFollowSymlink,
-        ).context("utimensat")?;
-
-        // suid/sgid gets cleared after chown
-        if permissions.contains(Mode::S_ISUID) || permissions.contains(Mode::S_ISGID) {
-            fchmodat(
-                Some(dest_dirfd.as_raw_fd()),
-                entry.name,
-                permissions,
-                FchmodatFlags::NoFollowSymlink,
-            ).context("fchmodat")?;
-        }
-
-        src.for_each_xattr(|name, value| {
-            // slowpath: most files only have 0 or 1 xattrs, so need to dedupe fd path creation
-            with_fd_path(dest_dirfd, entry.name, |path_cstr| {
-                lsetxattr(path_cstr, name, value, 0)
-            })
-        }).context("listxattr/setxattr")?;
-    }
-
-    // file contents: only support reflinking
-    if src.file_type == FileType::Regular {
-        copy_regular_file_contents(&src.st, src.fd.as_ref().unwrap(), dest_fd.as_ref().unwrap())?;
-    }
-
-    // recurse into non-empty directories
-    if src.has_children() {
-        walk_dir(src.fd.as_ref().unwrap(), src.nents_hint(), dest_fd.as_ref().unwrap(), buffer_stack)?;
-    }
-
-    Ok(())
-}
-
-fn walk_dir(
-    src_dirfd: &OwnedFd,
-    src_nents_hint: Option<usize>,
-    dest_dirfd: &OwnedFd,
-    buffer_stack: &BufferStack,
-) -> anyhow::Result<()> {
-    for_each_getdents(src_dirfd, src_nents_hint, buffer_stack, |entry| {
-        do_one_entry(src_dirfd, dest_dirfd, &entry, buffer_stack).map_err(|e| {
-            if e.is::<nix::Error>() {
-                // nix::Error = root cause
-                // start chain: "PATH: ERROR"
-                anyhow!("{}: {}", entry.name.to_string_lossy(), e)
-            } else {
-                // as we unwind the directory stack, prepend dirs to error
-                // chain: "DIR/CHILD: ERROR"
-                anyhow!("{}/{}", entry.name.to_string_lossy(), e)
-            }
-        })
-    })?;
-
-    Ok(())
-}
-
 fn main() -> anyhow::Result<()> {
     // we need control over all permissions bits
     umask(Mode::empty());
@@ -303,8 +326,8 @@ fn main() -> anyhow::Result<()> {
     };
 
     // walk dirs
-    let buffer_stack = BufferStack::new()?;
-    walk_dir(&src_dirfd, None, &dest_dirfd, &buffer_stack)
+    let ctx = CopyContext::new()?;
+    ctx.walk_dir(&src_dirfd, None, &dest_dirfd)
         .map_err(|e| anyhow!("{}/{}", src_dir, e))?;
 
     Ok(())
