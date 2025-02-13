636b4a0fa9cf4462a87a7d7f75f6b4e16abfc86a
Danny Lin <danny@kdrag0n.dev>
2025-02-12 22:08:31 -0800

    exp: network: time commands in matrix test

--- exp/network/domains-matrix.sh
+++ exp/network/domains-matrix.sh
@@ -16,111 +16,113 @@ k8s_pod=k8s_controller_ingress-nginx-controller-5bdc4f464b-xbdxb_ingress-nginx_1
 # - k8s ingress
 # - k8s pod
 
+cmd="time curl"
+
 for domain_port in ${DOMAIN_PORTS[@]}; do
     domain=${domain_port%:*}
     port=${domain_port#*:}
 
     # from docker machine
     echo -e "\n** $domain:80 from docker machine, ipv4"
-    orb debug _docker curl -v -4 http://${domain}
+    orb debug _docker $cmd -v -4 http://${domain}
     echo -e "\n** $domain:$port from docker machine, ipv4"
-    orb debug _docker curl -v -4 http://${domain}:$port
+    orb debug _docker $cmd -v -4 http://${domain}:$port
     if [ "$IPV6_ENABLED" = true ]; then
         echo -e "\n** $domain from docker machine, ipv6"
-        orb debug _docker curl -v -6 http://${domain}
+        orb debug _docker $cmd -v -6 http://${domain}
         echo -e "\n** $domain:$port from docker machine, ipv6"
-        orb debug _docker curl -v -6 http://${domain}:$port
+        orb debug _docker $cmd -v -6 http://${domain}:$port
     fi
     echo -e "\n** $domain from docker machine, ipv4 https"
-    orb debug _docker curl -v -4 https://${domain}
+    orb debug _docker $cmd -v -4 https://${domain}
     if [ "$IPV6_ENABLED" = true ]; then
         echo -e "\n** $domain from docker machine, ipv6 https"
-        orb debug _docker curl -v -6 https://${domain}
+        orb debug _docker $cmd -v -6 https://${domain}
     fi
 
     # from self container, hairpinning
     echo -e "\n** $domain:80 from container, ipv4"
-    orb debug $self_container curl -v -4 http://${domain}
+    orb debug $self_container $cmd -v -4 http://${domain}
     echo -e "\n** $domain:$port from container, ipv4"
-    orb debug $self_container curl -v -4 http://${domain}:$port
+    orb debug $self_container $cmd -v -4 http://${domain}:$port
     if [ "$IPV6_ENABLED" = true ]; then
         echo -e "\n** $domain from container, ipv6"
-        orb debug $self_container curl -v -6 http://${domain}
+        orb debug $self_container $cmd -v -6 http://${domain}
         echo -e "\n** $domain:$port from container, ipv6"
-        orb debug $self_container curl -v -6 http://${domain}:$port
+        orb debug $self_container $cmd -v -6 http://${domain}:$port
     fi
     echo -e "\n** $domain from container, ipv4 https"
-    orb debug $self_container curl -v -4 https://${domain}
+    orb debug $self_container $cmd -v -4 https://${domain}
     if [ "$IPV6_ENABLED" = true ]; then
         echo -e "\n** $domain from container, ipv6 https"
-        orb debug $self_container curl -v -6 https://${domain}
+        orb debug $self_container $cmd -v -6 https://${domain}
     fi
 
     # from other container, hairpinning
     echo -e "\n** $domain:80 from other container, ipv4"
-    orb debug $other_container curl -v -4 http://${domain}
+    orb debug $other_container $cmd -v -4 http://${domain}
     echo -e "\n** $domain:$port from other container, ipv4"
-    orb debug $other_container curl -v -4 http://${domain}:$port
+    orb debug $other_container $cmd -v -4 http://${domain}:$port
     if [ "$IPV6_ENABLED" = true ]; then
         echo -e "\n** $domain from other container, ipv6"
-        orb debug $other_container curl -v -6 http://${domain}
+        orb debug $other_container $cmd -v -6 http://${domain}
         echo -e "\n** $domain:$port from other container, ipv6"
-        orb debug $other_container curl -v -6 http://${domain}:$port
+        orb debug $other_container $cmd -v -6 http://${domain}:$port
     fi
     echo -e "\n** $domain from other container, ipv4 https"
-    orb debug $other_container curl -v -4 https://${domain}
+    orb debug $other_container $cmd -v -4 https://${domain}
     if [ "$IPV6_ENABLED" = true ]; then
         echo -e "\n** $domain from other container, ipv6 https"
-        orb debug $other_container curl -v -6 https://${domain}
+        orb debug $other_container $cmd -v -6 https://${domain}
     fi
 
     # from linux machine
     echo -e "\n** $domain:80 from linux machine, ipv4"
-    orb -m $machine curl -v -4 http://${domain}
+    orb -m $machine $cmd -v -4 http://${domain}
     echo -e "\n** $domain:$port from linux machine, ipv4"
-    orb -m $machine curl -v -4 http://${domain}:$port
+    orb -m $machine $cmd -v -4 http://${domain}:$port
     if [ "$IPV6_ENABLED" = true ]; then
         echo -e "\n** $domain from linux machine, ipv6"
-        orb -m $machine curl -v -6 http://${domain}
+        orb -m $machine $cmd -v -6 http://${domain}
         echo -e "\n** $domain:$port from linux machine, ipv6"
-        orb -m $machine curl -v -6 http://${domain}:$port
+        orb -m $machine $cmd -v -6 http://${domain}:$port
     fi
     echo -e "\n** $domain from linux machine, ipv4 https"
-    orb -m $machine curl -v -4 https://${domain}
+    orb -m $machine $cmd -v -4 https://${domain}
     if [ "$IPV6_ENABLED" = true ]; then
         echo -e "\n** $domain from linux machine, ipv6 https"
-        orb -m $machine curl -v -6 https://${domain}
+        orb -m $machine $cmd -v -6 https://${domain}
     fi
 
     # from pod
     echo -e "\n** $domain:80 from pod, ipv4"
-    orb debug $k8s_pod curl -v -4 http://${domain}
+    orb debug $k8s_pod $cmd -v -4 http://${domain}
     echo -e "\n** $domain:$port from pod, ipv4"
-    orb debug $k8s_pod curl -v -4 http://${domain}:$port
+    orb debug $k8s_pod $cmd -v -4 http://${domain}:$port
     if [ "$IPV6_ENABLED" = true ]; then
         echo -e "\n** $domain from pod, ipv6"
-        orb debug $k8s_pod curl -v -6 http://${domain}
+        orb debug $k8s_pod $cmd -v -6 http://${domain}
         echo -e "\n** $domain:$port from pod, ipv6"
-        orb debug $k8s_pod curl -v -6 http://${domain}:$port
+        orb debug $k8s_pod $cmd -v -6 http://${domain}:$port
     fi
     echo -e "\n** $domain from pod, ipv4 https"
-    orb debug $k8s_pod curl -v -4 https://${domain}
+    orb debug $k8s_pod $cmd -v -4 https://${domain}
     if [ "$IPV6_ENABLED" = true ]; then
         echo -e "\n** $domain from pod, ipv6 https"
-        orb debug $k8s_pod curl -v -6 https://${domain}
+        orb debug $k8s_pod $cmd -v -6 https://${domain}
     fi
 
     # from host
     echo -e "\n** $domain:80 from host, ipv4"
-    curl -v -4 http://${domain}
+    $cmd -v -4 http://${domain}
     echo -e "\n** $domain:$port from host, ipv4"
-    curl -v -4 http://${domain}:$port
+    $cmd -v -4 http://${domain}:$port
     echo -e "\n** $domain from host, ipv6"
-    curl -v -6 http://${domain}
+    $cmd -v -6 http://${domain}
     echo -e "\n** $domain:$port from host, ipv6"
-    curl -v -6 http://${domain}:$port
+    $cmd -v -6 http://${domain}:$port
     echo -e "\n** $domain from host, ipv4 https"
-    curl -v -4 https://${domain}
+    $cmd -v -4 https://${domain}
     echo -e "\n** $domain from host, ipv6 https"
-    curl -v -6 https://${domain}
+    $cmd -v -6 https://${domain}
 done
