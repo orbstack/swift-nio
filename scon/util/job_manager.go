c2a8f08c0189a4667fa9d7f2b63e978ae544defe
Danny Lin <danny@kdrag0n.dev>
2025-02-13 01:14:10 -0800

    scon: fix job manager ctx.done goroutine leak

--- scon/util/job_manager.go
+++ scon/util/job_manager.go
@@ -15,27 +15,16 @@ type EntityJobManager struct {
 func NewEntityJobManager(ctx context.Context) *EntityJobManager {
 	ctx, cancel := context.WithCancel(ctx)
 	return &EntityJobManager{
-		globalWg: sync.WaitGroup{},
-		ctx:      ctx,
-		cancel:   cancel,
+		ctx:    ctx,
+		cancel: cancel,
 	}
 }
 
 func (m *EntityJobManager) Run(job func(ctx context.Context) error) error {
-	var thisWg sync.WaitGroup
-	thisWg.Add(1)
 	m.globalWg.Add(1)
+	defer m.globalWg.Done()
 
-	var err error
-	go func() {
-		defer thisWg.Done()
-		defer m.globalWg.Done()
-
-		err = job(m.ctx)
-	}()
-
-	thisWg.Wait()
-	return err
+	return job(m.ctx)
 }
 
 func (m *EntityJobManager) RunContext(ctx context.Context, job func(ctx context.Context) error) error {
@@ -44,7 +33,10 @@ func (m *EntityJobManager) RunContext(ctx context.Context, job func(ctx context.
 
 	return m.Run(func(managerCtx context.Context) error {
 		go func() {
-			<-managerCtx.Done()
+			select {
+			case <-managerCtx.Done():
+			case <-ctx.Done():
+			}
 			cancel()
 		}()
 
