55507486fa191a039df2b6bb6fcd91a2784623d1
Danny Lin <danny@kdrag0n.dev>
2025-02-13 03:43:31 -0800

    bpf: pmon: fix program load on x86
    ❯ sudo bpftool prog load pmon_x86_bpfel.o /sys/fs/bpf/asd
    libbpf: prog 'nf_tables_newrule': BPF program load failed: -EINVAL
    libbpf: prog 'nf_tables_newrule': -- BEGIN PROG LOAD LOG --
    0: R1=ctx() R10=fp0
    ;  @ :453
    0: <invalid CO-RE relocation>
    failed to resolve CO-RE relocation <byte_off> [69] struct pt_regs.rax (0:10 @ offset 80)
    processed 1 insns (limit 1000000) max_states_per_insn 0 total_states 0 peak_states 0 mark_read 0
    -- END PROG LOAD LOG --
    libbpf: prog 'nf_tables_newrule': failed to load: -EINVAL
    libbpf: failed to load object 'pmon_x86_bpfel.o'
    Error: failed to load object file

--- scon/bpf/src/pmon.c
+++ scon/bpf/src/pmon.c
@@ -40,8 +40,39 @@ struct user_pt_regs {
 } __attribute__((preserve_access_index));
 #elif defined(bpf_target_x86)
 struct pt_regs {
-    unsigned long rax;
-} __attribute__((preserve_access_index));
+/*
+ * C ABI says these regs are callee-preserved. They aren't saved on kernel entry
+ * unless syscall needs a complete, fully filled "struct pt_regs".
+ */
+        unsigned long r15;
+        unsigned long r14;
+        unsigned long r13;
+        unsigned long r12;
+        unsigned long rbp;
+        unsigned long rbx;
+/* These regs are callee-clobbered. Always saved on kernel entry. */
+        unsigned long r11;
+        unsigned long r10;
+        unsigned long r9;
+        unsigned long r8;
+        unsigned long rax;
+        unsigned long rcx;
+        unsigned long rdx;
+        unsigned long rsi;
+        unsigned long rdi;
+/*
+ * On syscall entry, this is syscall#. On CPU exception, this is error code.
+ * On hw interrupt, it's IRQ number:
+ */
+        unsigned long orig_rax;
+/* Return frame for iretq */
+        unsigned long rip;
+        unsigned long cs;
+        unsigned long eflags;
+        unsigned long rsp;
+        unsigned long ss;
+/* top of stack page */
+};;
 #endif
 
 // #define DEBUG
