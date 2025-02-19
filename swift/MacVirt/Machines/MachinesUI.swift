4edb76ca7d1e720ebfa4e527b8551d62e5dc0b41
Cheru Berhanu <cheru@orbstack.dev>
2025-02-18 19:00:06 -0800

    swift: open machine terminal to $HOMEwd

--- swift/MacVirt/Machines/MachinesUI.swift
+++ swift/MacVirt/Machines/MachinesUI.swift
@@ -8,7 +8,8 @@ import Foundation
 extension ContainerRecord {
     func openInTerminal() async {
         do {
-            try await openTerminal(AppConfig.shellExe, ["-m", name])
+            // -w="" opens terminal in machine's home dir
+            try await openTerminal(AppConfig.shellExe, ["-m", name, "-w", ""])
         } catch {
             NSLog("Open terminal failed: \(error)")
         }
