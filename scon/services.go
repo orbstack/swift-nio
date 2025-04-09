7658c3f633069a38f8e40d71b929c94e8c82849c
Danny Lin <danny@kdrag0n.dev>
2025-04-09 01:05:38 -0700

    scon: fix TCP keepalives being enabled on RPC server
    this was a mistake when migrating to readyevents

--- scon/services.go
+++ scon/services.go
@@ -4,12 +4,13 @@ import (
 	"net"
 	"strconv"
 
+	"github.com/orbstack/macvirt/scon/util/netx"
 	"github.com/orbstack/macvirt/vmgr/vnet/netconf"
 	"github.com/orbstack/macvirt/vmgr/vnet/services/readyevents/readyclient"
 )
 
 func listenAndReportReady(network string, service string, host string, port uint16) (net.Listener, error) {
-	listener, err := net.Listen(network, net.JoinHostPort(host, strconv.Itoa(int(port))))
+	listener, err := netx.Listen(network, net.JoinHostPort(host, strconv.Itoa(int(port))))
 	if err != nil {
 		return nil, err
 	}
