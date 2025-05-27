ff473fd6c4f3b9cc85801142697205ba80ebfc75
emmie <emmie@emmie.zip>
2025-05-27 00:55:48 -0600

    scon: fix portprober reusing waitgroup
    this fixes orbstack/orbstack#1859
    
    waitgroups cannot be added to while a wait is active
    
    � system | panic: sync: WaitGroup is reused before previous Wait has returned
    � system |
    � system | goroutine 6644 [running]:
    � system | sync.(*WaitGroup).Wait(0x40033f2358)
    � system | 	sync/waitgroup.go:120 +0x80
    � system | github.com/orbstack/macvirt/scon/util/portprober.(*HostProbe).Wait(...)
    � system | 	github.com/orbstack/macvirt/scon/util/portprober/port_prober.go:73
    � system | github.com/orbstack/macvirt/scon/util/portprober.(*HostProbe).Probe(0x40033f2300, 0x4000296510?)
    � system | 	github.com/orbstack/macvirt/scon/util/portprober/port_prober.go:146 +0x78
    � system | github.com/orbstack/macvirt/scon/domainproxy.(*DomainTLSProxy).probeUpstream(0x4000298140, 0x40034941b0, {{0x1, {0x4002f1d540, 0x40}}, {0x4004528a50, 0x3, 0x3}, {0x4004514970, 0x10, ...}, ...})
    � system | 	github.com/orbstack/macvirt/scon/domainproxy/nfqueue.go:258 +0x484
    � system | github.com/orbstack/macvirt/scon/domainproxy.(*DomainTLSProxy).probeHost(0x4000298140, {{0x0, 0xffffc613f804}, {0x40000ea390}}, {{0x40035ac5b8?, 0x4001817000?}, {0x40000ea390?}})
    � system | 	github.com/orbstack/macvirt/scon/domainproxy/nfqueue.go:133 +0x384
    � system | github.com/orbstack/macvirt/scon/domainproxy.(*DomainTLSProxy).handleNfqueuePacket(0x4000298140, {0x40047b8d38, 0x40047b8d57, 0x0, 0x40047b8d74, 0x40047b8d64, 0x40047b8d6c, 0x0, 0x0, 0x40035ac588, ...})
    � system | 	github.com/orbstack/macvirt/scon/domainproxy/nfqueue.go:101 +0x3fc
    � system | github.com/orbstack/macvirt/scon/domainproxy.(*DomainTLSProxy).startQueue.func1({0x4003478f28?, 0x100000000?}, 0x4003478f28?, {{0x40047b8d38, 0x40047b8d57, 0x0, 0x40047b8d74, 0x40047b8d64, 0x40047b8d6c, 0x0, ...}, ...})
    � system | 	github.com/orbstack/macvirt/scon/domainproxy/nfqueue.go:40 +0x4c
    � system | created by github.com/orbstack/macvirt/scon/util.RunNfqueue.func1 in goroutine 85
    � system | 	github.com/orbstack/macvirt/scon/util/nfqueue.go:74 +0x114

--- scon/util/portprober/port_prober.go
+++ scon/util/portprober/port_prober.go
@@ -28,7 +28,10 @@ type HostProbe struct {
 	ctx       context.Context
 	cancel    *util.CancelAfter
 
-	wg          sync.WaitGroup
+	numActive     int
+	completed     bool
+	completedCond *sync.Cond
+
 	activeHTTP  map[uint16]struct{}
 	activeHTTPS map[uint16]struct{}
 
@@ -48,7 +51,7 @@ type HostProbeOptions struct {
 func NewHostProbe(ctx context.Context, opts HostProbeOptions) *HostProbe {
 	ctx, cancel := context.WithCancel(ctx)
 
-	return &HostProbe{
+	p := &HostProbe{
 		errFunc: opts.ErrFunc,
 
 		dialer:     opts.Dialer,
@@ -67,11 +70,20 @@ func NewHostProbe(ctx context.Context, opts HostProbeOptions) *HostProbe {
 			HTTPSPorts: make(map[uint16]struct{}),
 		},
 	}
+
+	p.completedCond = sync.NewCond(&p.mu)
+
+	return p
 }
 
 func (p *HostProbe) Wait() *ProbeResult {
-	p.wg.Wait()
-	// safe to return probeResult because everything is done
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	for !p.completed {
+		p.completedCond.Wait()
+	}
+
 	return p.probeResult
 }
 
@@ -80,6 +92,10 @@ type protocolProbeFunc func(ctx context.Context, dialer *net.Dialer, host string
 func (p *HostProbe) startProtocolProbe(port uint16, probeFunc protocolProbeFunc, activeMap map[uint16]struct{}, successMap map[uint16]struct{}) {
 	p.mu.Lock()
 	defer p.mu.Unlock()
+
+	if p.completed {
+		return
+	}
 
 	if p.ctx.Err() != nil {
 		// don't start new probes if we're already done
@@ -98,10 +114,8 @@ func (p *HostProbe) startProtocolProbe(port uint16, probeFunc protocolProbeFunc,
 
 	activeMap[port] = struct{}{}
 
-	p.wg.Add(1)
+	p.numActive++
 	go func() {
-		defer p.wg.Done()
-
 		// all of these are static so safe to access from concurrent context without lock
 		portOpen, err := probeFunc(p.ctx, p.dialer, p.host, port, p.serverName)
 		if err != nil {
@@ -117,30 +131,36 @@ func (p *HostProbe) startProtocolProbe(port uint16, probeFunc protocolProbeFunc,
 			successMap[port] = struct{}{}
 			p.cancel.CancelAfter(p.graceTime)
 		}
+
+		p.numActive--
+		if p.numActive == 0 {
+			p.completed = true
+			p.completedCond.Broadcast()
+		}
 	}()
 }
 
-func (p *HostProbe) StartHTTPProbe(port uint16) {
+func (p *HostProbe) startHTTPProbe(port uint16) {
 	p.startProtocolProbe(port, probePortHTTP, p.activeHTTP, p.probeResult.HTTPPorts)
 }
 
-func (p *HostProbe) StartHTTPSProbe(port uint16) {
+func (p *HostProbe) startHTTPSProbe(port uint16) {
 	p.startProtocolProbe(port, probePortHTTPS, p.activeHTTPS, p.probeResult.HTTPSPorts)
 }
 
-func (p *HostProbe) StartProbe(port uint16) {
+func (p *HostProbe) startProbe(port uint16) {
 	// to avoid confusing behavior with servers (e.g. Traefik) that accept both HTTP and HTTPS on both 80 and 443, disallow **443 ports for HTTP, and disallow ***80 ports for HTTPS
 	if port%1000 != 443 {
-		p.StartHTTPProbe(port)
+		p.startHTTPProbe(port)
 	}
 	if port%1000 != 80 {
-		p.StartHTTPSProbe(port)
+		p.startHTTPSProbe(port)
 	}
 }
 
 func (p *HostProbe) Probe(ports map[uint16]struct{}) *ProbeResult {
 	for port := range ports {
-		p.StartProbe(port)
+		p.startProbe(port)
 	}
 
 	return p.Wait()
