3a37f3d47b7289097fbcd4a591725c7503beaf80
Danny Lin <danny@kdrag0n.dev>
2025-06-03 01:31:35 -0700

    scli: update "orb docker" message

--- scon/cmd/scli/cmd/docker.go
+++ scon/cmd/scli/cmd/docker.go
@@ -10,12 +10,11 @@ func init() {
 
 var dockerCmd = &cobra.Command{
 	Use:   "docker",
-	Short: "Show commands for using Docker",
-	Long: `The "orb" and "orbctl" commands are for managing OrbStack and its machines.
+	Short: "Extension commands for Docker",
+	Long: `The "orb" and "orbctl" commands are primarily for managing OrbStack and its Linux machines.
 
-To build and run containers and manage anything related to Docker, use the "docker" command directly from macOS:
-    docker
-There is no need to use machines if you only want to use Docker.
+To build and run containers and manage anything related to Docker, use the "docker" command directly from macOS.
+You don't need a Linux machine to use Docker.
 
 For example, to start an example container:
     docker run -p 80:80 docker/getting-started
@@ -25,13 +24,12 @@ Compose and buildx are also included:
     docker compose
     docker buildx
 
-To migrate containers, volumes, and images from Docker Desktop:
-    orb migrate docker
-
-OrbStack's cluster is configured as the "orbstack" context.
+OrbStack's Docker engine is configured as the "orbstack" context.
 To prevent OrbStack from changing the active Docker context automatically:
 	orb config set docker.set_context false
 
 For more info: https://orb.cx/docker
+
+Below are OrbStack commands that *extend* the core Docker CLI functionality.
 `,
 }
