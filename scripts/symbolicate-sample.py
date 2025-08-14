3f2ff5eefce6301999b82f3e6ef27a62acaab3c3
Danny Lin <danny@kdrag0n.dev>
2025-08-14 03:03:06 -0700

    scripts: symbolicate: 300x faster, support inline frames

--- scripts/symbolicate-sample.py
+++ scripts/symbolicate-sample.py
@@ -2,7 +2,7 @@
 
 import argparse
 import subprocess
-from tqdm import tqdm
+import collections
 
 parser = argparse.ArgumentParser(description="Symbolicate a sample file")
 parser.add_argument("sample_file", help="Path to the sample file")
@@ -12,9 +12,9 @@ args = parser.parse_args()
 with open(args.sample_file, "r") as f, open(
     args.sample_file + ".symbolicated", "w"
 ) as out_f:
-    num_lines = sum(1 for _ in f)
-    f.seek(0)
-    for line in tqdm(f, total=num_lines):
+    # collect all addrs
+    all_addrs = collections.defaultdict(set)
+    for line in f:
         if "???" in line and "load address" in line:
             load_address = line[
                 line.index("load address") + len("load address") :
@@ -24,9 +24,34 @@ with open(args.sample_file, "r") as f, open(
                 load_address.index("[") + 1 : load_address.index("]")
             ]
 
-            output = subprocess.check_output(
-                ["atos", "-o", args.dsym, "-l", base_addr, end_addr]
-            )
-            symbol = output.decode().strip()
-            line = line[: line.index("???")] + symbol + line[line.index("  [") :]
+            all_addrs[base_addr].add(end_addr)
         out_f.write(line)
+
+    # run atos on everything
+    all_addr_mappings = {}
+    for base_addr, end_addrs in all_addrs.items():
+        end_addrs_list = list(end_addrs)
+        output = subprocess.check_output(
+            ["atos", "-i", "-o", args.dsym, "-l", base_addr, *end_addrs]
+        )
+        for i, lookups in enumerate(output.decode().strip().split("\n\n")):
+            symbols = lookups.split("\n")[::-1]
+            all_addr_mappings[(base_addr, end_addrs_list[i])] = symbols
+
+    f.seek(0)
+    for line in f:
+        if "???" in line and "load address" in line:
+            load_address = line[
+                line.index("load address") + len("load address") :
+            ].strip()
+            base_addr = load_address[: load_address.index(" ")]
+            end_addr = load_address[
+                load_address.index("[") + 1 : load_address.index("]")
+            ]
+
+            symbols = all_addr_mappings[(base_addr, end_addr)]
+            indent_level = line.index("???")
+            symbols_str = ("\n" + " " * indent_level).join(symbols)
+            line = line[: indent_level] + symbols_str + line[line.index("  [") :]
+        out_f.write(line)
+        print(line, end="")
